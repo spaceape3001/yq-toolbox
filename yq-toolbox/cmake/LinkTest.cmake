6f50e4efccc830b57017b6d142f2e0263b4c17c0
spaceape3001 <spaceape3001@users.noreply.github.com>
2022-07-22 07:41:27 +0000

    Rename complete

--- yq-toolbox/cmake/LinkTest.cmake
+++ yq-toolbox/cmake/LinkTest.cmake
@@ -6,7 +6,7 @@
 
 MACRO(LinkTest Lib )
     IF(GCC)
-        add_executable( lt_${Lib} ${CMAKE_SOURCE_DIR}/yq-utility/cmake/linktest.cpp )
+        add_executable( lt_${Lib} ${CMAKE_SOURCE_DIR}/yq-toolbox/cmake/linktest.cpp )
         target_link_libraries(lt_${Lib} ${Lib}) 
         set_target_properties(lt_${Lib} PROPERTIES RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/linktest )
     ENDIF()
