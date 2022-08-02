33c099b6fce5e387945e74193071534704af72bb
spaceape3001 <spaceape3001@users.noreply.github.com>
2022-08-02 07:23:51 +0000

    Doxygen

--- api/math/SegmentData.hpp
+++ api/math/SegmentData.hpp
@@ -7,6 +7,11 @@
 #pragma once
 
 namespace yq {
+
+    /*! \brief Data for a segment
+    
+        \note Can be the vertex themselves, or other data.
+    */
     template <typename V>
     struct SegmentData {
         V   a, b;
