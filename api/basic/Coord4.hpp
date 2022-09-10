bd1702f2bf1080dfeda50aef31e4eb3787e3af73
spaceape3001 <spaceape3001@users.noreply.github.com>
2022-09-10 07:26:00 +0000

    Doxygen

--- api/basic/Coord4.hpp
+++ api/basic/Coord4.hpp
@@ -10,13 +10,20 @@
 
 namespace yq {
 
+    /*! \brief Four dimensional coordinate
+    
+        This is expanded in four diemnsions for the four-diemsnional coordinate.
+    */
     template <typename T>
     struct Coord<T,4> {
         using component_type    = T;
         static constexpr const uint8_t  DIMS    = 4;
         T       i, j, k, l;
+
+        //! Defaulted equality  operator
         constexpr bool operator==(const Coord&) const noexcept = default;
 
+        //! Conversion operator to other 4-dimensional coordinates
         template <typename U>
         requires (!std::is_same_v<T,U>)
         explicit constexpr operator Coord<U,4>() const noexcept
@@ -24,6 +31,7 @@ namespace yq {
             return { (U) i, (U) j, (U) k, (U) l };
         }
 
+        //! Allows for uniform coordinate assignments.
         Coord& operator=(T v)
         {
             i = j = k = l = v;
@@ -35,12 +43,14 @@ namespace yq {
     //  --------------------------------------------------------
     //  COMPOSITION
 
+    /*! \brief Composes a four dimensional coordinate from arguments */
     template <typename T>
     constexpr Coord4<T>    coord(T i, std::type_identity_t<T> j, std::type_identity_t<T> k, std::type_identity_t<T> l) noexcept
     {
         return { i, j, k, l };
     }
     
+    /*! \brief Construct a uniform 4-dimensional coordinate */
     template <typename T>
     constexpr Coord4<T>    coord4(T i)
     {
@@ -51,6 +61,7 @@ namespace yq {
     //  --------------------------------------------------------
     //  BASIC FUNCTIONS
 
+    /*! \brief Max of two coordinates, done by element */
     template <typename T>
     constexpr Coord4<T> max(const Coord4<T>&a, const Coord4<T>& b)
     {
@@ -62,6 +73,7 @@ namespace yq {
         };
     }
 
+    /*! \brief Min of two coordinates, done by element */
     template <typename T>
     constexpr Coord4<T> min(const Coord4<T>&a, const Coord4<T>& b)
     {
@@ -73,12 +85,14 @@ namespace yq {
         };
     }
 
+    /*! \brief Product of the components */
     template <typename T>
     constexpr auto product(const Coord4<T>& a)
     {
         return a.i*a.j*a.k*a.l;
     }
 
+    /*! \brief Sum of the components */
     template <typename T>
     constexpr T sum(const Coord4<T>& a)
     {
@@ -92,6 +106,7 @@ namespace yq {
     //  --------------------------------------------------------
     //  NEGATIVE
 
+    //! Negate the coordinate
     template <typename T>
     constexpr Coord4<T>    operator-(const Coord4<T>& a)
     {
@@ -106,12 +121,14 @@ namespace yq {
     //  --------------------------------------------------------
     //  ADDITION
     
+    //! Add two coordinates together
     template <typename T>
     constexpr Coord4<T> operator+(const Coord4<T>&a, const Coord4<T>&b)
     {
         return { a.i+b.i, a.j+b.j, a.k+b.k, a.l+b.l };
     }
     
+    //! Increment left coordinate with right
     template <typename T>
     Coord4<T>&  operator+=(Coord4<T>& a, const Coord4<T>& b)
     {
@@ -125,14 +142,15 @@ namespace yq {
 
     //  --------------------------------------------------------
     //  SUBTRACTION
-
     
+    //! Subtract two coordinates
     template <typename T>
     constexpr Coord4<T> operator-(const Coord4<T>&a, const Coord4<T>&b)
     {
         return { a.i-b.i, a.j-b.j, a.k-b.k, a.l-b.l };
     }
     
+    //! Decrement the left coordinate with right
     template <typename T>
     Coord4<T>&  operator-=(Coord4<T>& a, const Coord4<T>& b)
     {
@@ -147,6 +165,7 @@ namespace yq {
     //  --------------------------------------------------------
     //  MULTIPLICATION
 
+    //! Scale the coordinate
     template <typename T, typename U>
     requires (std::is_arithmetic_v<T>)
     constexpr Coord4<decltype(T()*U())> operator*(T a, const Coord4<U>&b)
@@ -154,6 +173,7 @@ namespace yq {
         return { a*b.i, a*b.j, a*b.k, a*b.l };
     }
     
+    //! Scale the coordinate
     template <typename T, typename U>
     requires (std::is_arithmetic_v<U>)
     constexpr Coord4<decltype(T()*U())> operator*(const Coord4<T>& a, U b)
@@ -161,6 +181,7 @@ namespace yq {
         return { a.i*b, a.j*b, a.k*b, a.l*b };
     }
 
+    //! Scale the coordinate
     template <typename T, typename U>
     requires (std::is_arithmetic_v<U> && std::is_same_v<T, decltype(T()*U())>)
     Coord4<T>& operator*=(Coord4<T>& a, U b)
@@ -172,12 +193,14 @@ namespace yq {
         return a;
     }
 
+    //! Multiplies two coordinates together, term by term
     template <typename T, typename U>
     constexpr Coord4<decltype(T()*U())> operator*(const Coord4<T>& a, const Coord4<U>& b)
     {
         return { a.i*b.i, a.j*b.j, a.k*b.k, a.l*b.l };
     }
     
+    //! Self-Multiplies left coordinate with right, term by term
     template <typename T, typename U>
     requires (std::is_same_v<T, decltype(T()*U())>)
     Coord4<T>& operator*=(Coord4<T>&a, const Coord4<U>& b)
@@ -193,6 +216,7 @@ namespace yq {
     //  --------------------------------------------------------
     //  DIVISION
 
+    //! Reduces the cooordinate, returns result
     template <typename T, typename U>
     requires (std::is_arithmetic_v<U>)
     constexpr Coord4<decltype(T()/U())> operator/(const Coord4<T>& a, U b)
@@ -200,6 +224,7 @@ namespace yq {
         return { a.i/b, a.j/b, a.k/b, a.l/b };
     }
 
+    //! Reduces the cooordinate in place, returns result
     template <typename T, typename U>
     requires (std::is_arithmetic_v<U> && std::is_same_v<T, decltype(T()/U())>)
     Coord4<T>& operator/=(Coord4<T>& a, U b)
@@ -211,12 +236,14 @@ namespace yq {
         return a;
     }
 
+    //! Divides two coordinates, term by term
     template <typename T, typename U>
     constexpr Coord4<decltype(T()/U())> operator/(const Coord4<T>& a, const Coord4<U>& b)
     {
         return { a.i/b.i, a.j/b.j, a.k/b.k, a.l/b.l };
     }
 
+    //! Self divides left coordinate by right
     template <typename T, typename U>
     requires (std::is_same_v<T, decltype(T()/U())>)
     Coord4<T>& operator/=(Coord4<T>& a, const Coord4<U>& b)
@@ -253,18 +280,21 @@ namespace yq {
     //  --------------------------------------------------------
     //  CONDITIONAL INCLUDES
 
+    //! Helper to stream out a coordinate
     template <typename S, typename I>
     S&  as_stream(S& s, const Coord4<I>& v)
     {
         return s << "[" << v.i << "," << v.j << "," << v.k << "," << v.l << "]";
     }
     
+    //! Helper to stream out a coordinate
     template <typename T>
     Stream& operator<<(Stream&s, const Coord4<T>& v)
     {
         return as_stream(s, v);
     }
 
+    //! Helper to log out a coordinate
     template <typename T>
     log4cpp::CategoryStream& operator<<(log4cpp::CategoryStream& s, const Coord4<T>& v)
     {
