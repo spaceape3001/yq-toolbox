3b5193e636fdcb1b67afab1056349e5664097723
spaceape3001 <spaceape3001@users.noreply.github.com>
2022-09-16 05:00:45 +0000

    Doxygen

--- api/basic/Guarded.hpp
+++ api/basic/Guarded.hpp
@@ -8,28 +8,57 @@
 #include <tbb/spin_rw_mutex.h>
 
 namespace yq {
+
+    /*! \brief Thread safe data access
+        
+        This class bundles a type with a R/W mutex to assure thread-safe access
+        to its contents.
+    */
     template <typename T>
     class Guarded {
     public:
 
+        //! Default constructor
         Guarded() {}
+        
+        //! Constructs with data 
+        //! (Note constructor itself is *NOT* thread safe)
         Guarded(const T& v) : m_data(v) {}
         
+        //! Assigns the data (under mutex) by copying it in
         Guarded&    operator=(const T& v)
         {
+            //  By copying it into a temporary, we marshall any expensive destructors
+            //  to fire *outside* the mutex lock.  Instead, only the move operators should be invoked.
+            T       old = v;    
             {
                 tbb::spin_rw_mutex::scoped_lock _lock(m_mutex, true);
-                m_data      = v;
+                std::swap(old, m_data);
             }
             return *this;
         }
         
+        //! Assigns the data (under mutex) by moving it in
+        Guarded&    operator=(T&& v)
+        {
+            //  By copying it into a temporary, we marshall any expensive destructors
+            //  to fire *outside* the mutex lock.  Instead, only the move operators should be invoked.
+            T       old = std::move(v);
+            {
+                tbb::spin_rw_mutex::scoped_lock _lock(m_mutex, true);
+                std::swap(old, m_data);
+            }
+            return *this;
+        }
+
+        //! Gets a COPY of the data (under read mutex)
         T   get() const
         {
             tbb::spin_rw_mutex::scoped_lock _lock(m_mutex, false);
             return m_data;
         }
         
+        //! Gets a COPY of the data (under read mutex)
         operator T() const
         {
             tbb::spin_rw_mutex::scoped_lock _lock(m_mutex, false);
