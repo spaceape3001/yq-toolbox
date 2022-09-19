62a1ef622e1029f5768cdb56b623af44fa33a991
spaceape3001 <spaceape3001@users.noreply.github.com>
2022-09-19 21:10:37 +0000

    Doxygen

--- api/basic/Concat.hpp
+++ api/basic/Concat.hpp
@@ -7,5 +7,8 @@
 #pragma once
 
 //  To use preprocessor right, for concatenation, the double-invoke is required (nuiance)
+
+//! Concatenate two things together, this implementation required for the macro magic
 #define YQ_CONCAT_IMPL(a, b) a##b
+//! Concatenate two things together, a second macro has to be called for this to work.
 #define YQ_CONCAT(a, b) YQ_CONCAT_IMPL(a,b)
