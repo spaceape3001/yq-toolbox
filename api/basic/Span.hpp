ebe321fe9310635a44fd9b4045cdb380413f7e02
spaceape3001 <spaceape3001@users.noreply.github.com>
2022-09-27 20:16:38 +0000

    Doxygen

--- api/basic/Span.hpp
+++ api/basic/Span.hpp
@@ -7,6 +7,10 @@
 #pragma once
 
 namespace yq {
+    /*! \brief Span object
+    
+        This is a nominal span with from/to (instead of the data/size of std::span)
+    */
     template <typename T>
     struct Span {
         T       from, to;
