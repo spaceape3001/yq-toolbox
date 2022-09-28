c27109b87c5b087ac1d76e7c107faf88542f81f7
spaceape3001 <spaceape3001@users.noreply.github.com>
2022-09-28 07:48:43 +0000

    Doxygen

--- api/basic/IntRange.hpp
+++ api/basic/IntRange.hpp
@@ -8,8 +8,15 @@
 
 namespace yq {
 
+    /*! \brief Integer based range
+    
+        General Point of this is to have something compatible with a range-based for loop.  
+        But also useful to caputuring low & high, might be combined with a span at a future point.
+    */
     template <typename T>
     struct IntRange {
+    
+        //! Iterator to enable it to iterate
         class iterator {
         public:
         
@@ -33,25 +40,32 @@ namespace yq {
             T     value;
         };
 
+        //! Constant iterator
         typedef iterator const_iterator;
 
+        //! Starting iterator
         iterator begin() const
         {
             return iterator(low);
         }
         
+        //! Ending iterator
         iterator end() const
         {
             return iterator(high);
         }
         
+        //! Count between
         constexpr T     count() const { return high - low; }
         
+        //! True if empty
         bool            empty() const { return low == high; }
+        
+        //! TRUE if not empty
         bool            not_empty() const { return low != high; }
         
-        
-        bool operator==(const IntRange&) const = default;
+        //! Equality operator
+        constexpr bool operator==(const IntRange&) const noexcept = default;
 
         const T         low, high;
     };
