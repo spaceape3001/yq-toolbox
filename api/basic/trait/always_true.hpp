9e0aac88b3b2763f97080edca51856fe800dffda
spaceape3001 <spaceape3001@users.noreply.github.com>
2022-09-30 07:30:17 +0000

    Doxygen

--- api/basic/trait/always_true.hpp
+++ api/basic/trait/always_true.hpp
@@ -9,7 +9,9 @@
 
 namespace yq {
     namespace trait {
+        //! Structure that is always true regardless of the template arguments
         template <typename...> struct always_true : std::true_type {};
+        //! Boolean that is always true regardless of the template arguments
         template <typename...> static constexpr bool always_true_v    = true;
     }
 }
