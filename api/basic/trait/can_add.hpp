76eec86d7a497a00719741e524a2fa1ef9c8040e
spaceape3001 <spaceape3001@users.noreply.github.com>
2022-10-01 06:22:05 +0000

    Doxygen

--- api/basic/trait/can_add.hpp
+++ api/basic/trait/can_add.hpp
@@ -10,8 +10,13 @@
 
 namespace yq {
     namespace trait {
+        /*! \brief Trait to test for addition
+        
+            This trait will be TRUE if the type can add with itself.
+        */
         template <typename T, typename=void> struct can_add : public std::false_type {};
         template <typename T> struct can_add<T,sink_t<decltype(T()+T())>> : public std::true_type {};
+        //! \brief BOOLEAN to test if a type can add with itself
         template <typename T> static constexpr const bool can_add_v = can_add<T>::value;
     }
 }
