a475c2159bc8370168af9aeab8aa0fc2534c8bdb
spaceape3001 <spaceape3001@users.noreply.github.com>
2022-10-02 08:40:09 +0000

    Doxygen

--- api/basic/trait/can_two_divide.hpp
+++ api/basic/trait/can_two_divide.hpp
@@ -10,8 +10,20 @@
 
 namespace yq {
     namespace trait {
+        /*! \brief Tests for two items being dividable
+        
+            This tests if a type T() can be divided by U()
+            
+            \tparam[T]  Dividend type
+            \tparam[U]  Divisor type
+        */
         template <typename T, typename U, typename=void> struct can_two_divide : public std::false_type {};
         template <typename T, typename U> struct can_two_divide<T,U,sink_t<decltype(T()/U())>> : public std::true_type {};
+        
+        /*! \brief Boolean flag for two items being dividable
+            \tparam[T]  Dividend type
+            \tparam[U]  Divisor type
+        */
         template <typename T, typename U> static constexpr const bool can_two_divide_v = can_two_divide<T,U>::value;
     }
 }
