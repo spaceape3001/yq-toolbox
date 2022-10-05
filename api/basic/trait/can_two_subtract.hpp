50b817083d7f6b0077708306649d8ca4e2050140
spaceape3001 <spaceape3001@users.noreply.github.com>
2022-10-05 08:45:11 +0000

    Doxygen

--- api/basic/trait/can_two_subtract.hpp
+++ api/basic/trait/can_two_subtract.hpp
@@ -10,8 +10,15 @@
 
 namespace yq {
     namespace trait {
+        //! Tests for two types that can can add
+        //! \tparam T   left term type
+        //! \tparam U   right term type
         template <typename T, typename U, typename=void> struct can_two_subtract : public std::false_type {};
         template <typename T, typename U> struct can_two_subtract<T,U,sink_t<decltype(T()-U())>> : public std::true_type {};
+        
+        //! Boolean that's true if the two types can add
+        //! \tparam T   left term type
+        //! \tparam U   right term type
         template <typename T, typename U> static constexpr const bool can_two_subtract_v = can_two_subtract<T,U>::value;
     }
 }
