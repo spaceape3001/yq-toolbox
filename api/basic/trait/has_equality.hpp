c3f6c244bb55d44ce0d880bcd22519fd930109b8
spaceape3001 <spaceape3001@users.noreply.github.com>
2022-10-07 04:20:51 +0000

    Doxygen

--- api/basic/trait/has_equality.hpp
+++ api/basic/trait/has_equality.hpp
@@ -10,8 +10,12 @@
 
 namespace yq {
     namespace trait {
+    
+        //! Tests for presents of equality on a type
         template <typename T, typename=void> struct has_equality : public std::false_type {};
         template <typename T> struct has_equality<T, sink_t<decltype(T()==T())>> : public std::true_type {};
+        
+        //! TRUE if the type has an equality operator defined for it.
         template <typename T> static constexpr const bool has_equality_v = has_equality<T>::value;
     }
 }
