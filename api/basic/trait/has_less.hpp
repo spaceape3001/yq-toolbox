3723fa3e4a632a909f45eff5e7a489d88c5e6c1b
spaceape3001 <spaceape3001@users.noreply.github.com>
2022-10-09 05:41:25 +0000

    Doxygen

--- api/basic/trait/has_less.hpp
+++ api/basic/trait/has_less.hpp
@@ -10,8 +10,10 @@
 
 namespace yq {
     namespace trait {
+        //! Tests for the presence of "less-than" capability for a type
         template <typename T, typename=void> struct has_less : public std::false_type {};
         template <typename T> struct has_less<T, sink_t<decltype(T()<T())>> : public std::true_type {};
+        //! TRUE if the type can be compared with a "less-than" operator
         template <typename T> static constexpr const bool has_less_v = has_less<T>::value;
     }
 }
