fb2eae7c0088bc98faed447d914481a3b3ce9edf
spaceape3001 <spaceape3001@users.noreply.github.com>
2022-10-11 08:38:46 +0000

    Doxygen

--- api/basic/trait/is_stdarray.hpp
+++ api/basic/trait/is_stdarray.hpp
@@ -12,10 +12,12 @@
 namespace yq {
     namespace trait {
 
+        //! Tests to see if type is std::array
         template <typename> struct is_stdarray : std::false_type {};
         template <typename T, std::size_t N>
         struct is_stdarray<std::array<T,N>> : std::true_type {};
         
+        //! TRUE if the given T is a std::array
         template <typename T>
         inline constexpr bool is_stdarray_v = is_stdarray<T>::value;
 
