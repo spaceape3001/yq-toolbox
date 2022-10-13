af44b18520ac5483416faa0210c7b8bfd29fdff2
spaceape3001 <spaceape3001@users.noreply.github.com>
2022-10-13 08:40:01 +0000

    Doxygen

--- api/basic/trait/is_stdvector.hpp
+++ api/basic/trait/is_stdvector.hpp
@@ -12,10 +12,12 @@
 namespace yq {
     namespace trait {
 
+        //! Tests a type to see if it's a std::vector
         template <typename> struct is_stdvector : std::false_type {};
         template <typename T, typename A>
         struct is_stdvector<std::vector<T,A>> : std::true_type {};
         
+        //! TRUE if a type is std::vector
         template <typename T>
         inline constexpr bool is_stdvector_v = is_stdvector<T>::value;
 
