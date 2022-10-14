cee790e3e817f2abdffee8fb4529afb390f59e41
spaceape3001 <spaceape3001@users.noreply.github.com>
2022-10-14 09:42:06 +0000

    Doxygen

--- api/basic/trait/is_template.hpp
+++ api/basic/trait/is_template.hpp
@@ -9,10 +9,12 @@
 namespace yq {
     namespace trait {
 
+        //! Tests to see if type T is a template
         template <typename> struct is_template : std::false_type {};
         template <template <typename...> class Tmpl, typename ...Args>
         struct is_template<Tmpl<Args...>> : std::true_type {};
         
+        //! TRUE if the given type is a template
         template <typename T>
         inline constexpr bool is_template_v = is_template<T>::value;
     }
