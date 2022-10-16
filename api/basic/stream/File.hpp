fa5ed297ddb4de15c0a5d8fd9eca4b05e9da3c7c
spaceape3001 <spaceape3001@users.noreply.github.com>
2022-10-16 07:53:40 +0000

    Doxygen & Traits
    Doxygen, and adding no-copy/no-move traits to the File stream object.

--- api/basic/stream/File.hpp
+++ api/basic/stream/File.hpp
@@ -7,6 +7,8 @@
 #pragma once
 
 #include <basic/Stream.hpp>
+#include <basic/trait/not_copyable.hpp>
+#include <basic/trait/not_moveable.hpp>
 #include <filesystem>
 #include <fstream>
 
@@ -15,19 +17,31 @@ namespace yq {
 
         /*! \brief Stream based on C++ ofstream
         */
-        class File : public Stream {
+        class File : public Stream, trait::not_copyable, trait::not_moveable {
         public:
-
+    
+            //! Default constructor
             File();
+            
+            //! Default destructor
             ~File();
             
+            //! Opens the specified filename
             bool        open(const std::filesystem::path&);
+            
+            //! Closes the stream
             void        close() override;
+            
+            //! True if open
             bool        is_open() const override;
             
+            //! Writes the specified data tot eh stream
             virtual bool    write(const char*z, size_t cb) override;
+            
+            //! Flushes the stream
             virtual void    flush() override;
             
+            //! Path of the stream
             const std::filesystem::path&   path() const { return m_path; }
             
         private:
