e395a56a255cf3569a42a4d8cae424e81afb0f4f
spaceape3001 <spaceape3001@users.noreply.github.com>
2022-10-20 08:42:05 +0000

    Doxygen
    Added doxygen and the no copy/move traits to the Text stream class.

--- api/basic/stream/Text.hpp
+++ api/basic/stream/Text.hpp
@@ -7,17 +7,27 @@
 #pragma once
 
 #include <basic/Stream.hpp>
+#include <basic/trait/not_copyable.hpp>
+#include <basic/trait/not_moveable.hpp>
 #include <string>
 
 namespace yq {
     class String;
     
     namespace stream {
-        class Text : public Stream {
+    
+        //! Wrapper to stream to a std::string
+        class Text : public Stream, trait::not_copyable, trait::not_moveable {
         public:
+        
+            //! Constructor that takes in a reference to ta strign
+            //! \note This must remain VALID the entire duration of this class
             Text(std::string& bytes) : m_bytes(bytes) {}
+            
+            //! Destructor
             ~Text(){}
 
+            //! Overload that appends to the string
             bool write(const char* buf, size_t cb) override;
 
         private:
