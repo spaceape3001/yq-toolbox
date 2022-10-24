d1ebd249dbf26b1e0a67e9962a4bd2d80201e890
spaceape3001 <spaceape3001@users.noreply.github.com>
2022-10-24 07:41:32 +0000

    Doxygen

--- api/basic/Signal.hpp
+++ api/basic/Signal.hpp
@@ -11,6 +11,7 @@
 
 namespace yq {
     
+    //! Function declaration for below
     template <typename ...Args>
     using Delegate = std::function<void(Args...)>;
 
@@ -20,24 +21,30 @@ namespace yq {
     template <typename ...Args>
     class Signal {
     public:
+    
+        //! Our function type
         using FN  = Delegate<Args...>;
     
+        //! Connects to the signal
         void    connect(FN f)
         {
             m_slots.push_back(f);
         }
         
+        //! Invokes the signal
         void    operator()(Args...a)
         {
             for(auto& i : m_slots)
                 i(a...);
         }
         
+        //! True if nobody has connected to the signal
         bool    empty() const 
         {
             return m_slots.empty();
         }
         
+        //! Clears the signal of all callers
         void    clear()
         {
             m_slots.clear();
