3f4f3265a37ac27c541541d73bbfe2c67ceeaa62
spaceape3001 <spaceape3001@users.noreply.github.com>
2022-10-25 07:59:44 +0000

    Doxygen

--- api/basic/UniqueID.hpp
+++ api/basic/UniqueID.hpp
@@ -10,11 +10,21 @@
 #include <basic/trait/not_copyable.hpp>
 
 namespace yq {
+
+    /*! \brief Class for a unique ID
+    
+        For those situations where a unique ID is required (but the actual order unimportant),
+        this is that class!
+    */
     class UniqueID : trait::not_copyable, trait::not_moveable {
     public:
+    
+        //! This instance's unique ID
         uint64_t    id() const { return m_id; }
         
     protected:
+    
+        //! Default constructor (that auto-generates a new ID)
         UniqueID();
     private:
         uint64_t    m_id;
