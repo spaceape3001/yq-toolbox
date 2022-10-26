c7a3082e93b7a2b12dc828eee97d9adb520b3a28
spaceape3001 <spaceape3001@users.noreply.github.com>
2022-10-26 08:16:19 +0000

    Doxygen

--- api/basic/Revision.hpp
+++ api/basic/Revision.hpp
@@ -10,14 +10,28 @@
 #include <basic/trait/not_copyable.hpp>
 
 namespace yq {
+
+    /*! \brief Class for revision counting
+    
+        When an object needs a "revision counting", this is the object.  
+        
+        An optional setting is to specify a "master" so if tracking an invididual component, it'll also 
+        trigger an update to the master's revision number too.
+    */
     class Revision : trait::not_copyable, trait::not_moveable {
     public:
     
+        //! Current revision number.
         uint64_t    revision() const { return m_revision; }
         
     protected:
+        /*! \brief Constructor
+        
+            \param[in] m    optioanl master
+        */
         Revision(Revision* m=nullptr);
         
+        //! Call this when something's changed, so that way the revision number is incremented.
         void        changed();
     private:
         uint64_t    m_revision;
