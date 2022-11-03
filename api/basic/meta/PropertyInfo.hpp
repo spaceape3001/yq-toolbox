54a2fbecc794fa1f572c5eec84969cc615be7ac7
spaceape3001 <spaceape3001@users.noreply.github.com>
2022-11-03 08:51:41 +0000

    Doxygen

--- api/basic/meta/PropertyInfo.hpp
+++ api/basic/meta/PropertyInfo.hpp
@@ -28,31 +28,71 @@ namespace yq {
         friend class PropSetter;
     public:
     
+        //! Our writer
         template <typename T> class Writer;
+        
+        //! Writer for statics
         template <typename T> class VarW;
+        
+        //! Writer for dynamics
         template <typename C, typename T> class PropW;
     
+        //! Our type information
         const TypeInfo&     type() const { return m_type; }
         
-        Any                 get(const void*) const;
+        //! Gets the property/variable
+        //! \param[in]  pointer Pointer to the object (can be null for statics)
+        Any                 get(const void* obj) const;
         
-        // write for saving
-        bool                write(const void*, Stream&) const;
+        //! Write for saving
+        //!
+        //! \param[in]  pointer Pointer to the object (can be null for statics)
+        //! \param[out]  str     Stream to write to
+        bool                write(const void* obj, Stream&str) const;
         
-        // write for printing
+        //! Write for printing
+        //!
+        //! \param[in]      pointer Pointer to the object (can be null for statics)
+        //! \param[in,out]  str     Stream to write to
         bool                print(const void*, Stream&) const;
         
-        bool                set(void*, const Any&) const;
-        bool                set(void*, std::string_view) const;
+        //! Sets the property with Any
+        //! \param[in,out]  pointer Pointer to the object (can be null for statics)
+        //! \param[in]      value   Any value to assign to the property
+        bool                set(void*obj, const Any&value) const;
+        //! Sets the property with string
+        //! \param[in,out]  pointer Pointer to the object (can be null for statics)
+        //! \param[in]      value   String value to assign to the property
+        bool                set(void*obj, std::string_view value) const;
         
+        //! Our getter
         const PropGetter*   getter() const { return m_getter; }
+
+        //! Our setter (may be NULL for read-only)
         const PropSetter*   setter() const { return m_setter; }
         
+        //! TRUE if this is marked as state.
+        //! 
+        //! A state property is one that needs to be saved/loaded to properly recreate the object.
         bool                is_state() const;
+        
+        //! TRUE if this is a global/static variabels
+        //!
+        //! A global or static is one that lives outside a particular object, but is instead defined singularly
+        //! For the entire application
         bool                is_static() const;
 
             // best know what you're doing....
-        PropertyInfo(std::string_view zName, const std::source_location& sl, const TypeInfo&, Meta*, options_t opts=0);
+            
+        /*! \brief Master Constructor 
+            \note   Only use directly this if you know what you're doing, otherwise, stay away
+            \param[in] zName    name of the property
+            \param[in] sl       Source location of the definition (usually auto-picked up by the meta declare helpers
+            \param[in] type     Data type for the property
+            \param[in] parent   Parent object this is apart of
+            \param[in] opts     Options
+        */
+        PropertyInfo(std::string_view zName, const std::source_location& sl, const TypeInfo& type, Meta* parent, options_t opts=0);
         
     private:
         const PropSetter*   m_setter    = nullptr;
