ffdedc3fc732fcaebbd9aa5bc9ce64f16847f934
spaceape3001 <spaceape3001@users.noreply.github.com>
2022-11-16 09:48:16 +0000

    Doxygen

--- api/math/Plane3.hpp
+++ api/math/Plane3.hpp
@@ -13,13 +13,23 @@ namespace yq {
     */
     template <typename T>
     struct Plane3 {
+        
+        //! Type of the component (ie captures our template argument)
         using component_type    = T;
+        
+        //! The underlying IEEE 754 data type
         using ieee754_type      = ieee754_t<T>;
+        
+        //! The relevant normal type
         using normal_type       = Normal3<ieee754_type>;
     
+        //! Normal
         normal_type     normal;
+        
+        //! Distance from the origin in the unit-space of T
         T               distance;
         
+        //! Defaulted comparison operator
         constexpr bool operator==(const Plane3&) const noexcept = default;
     };
 
