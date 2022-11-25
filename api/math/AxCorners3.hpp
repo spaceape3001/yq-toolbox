9cb373a0faa1ef353c3e675324982352629e7012
spaceape3001 <spaceape3001@users.noreply.github.com>
2022-11-25 10:23:05 +0000

    Doxygen

--- api/math/AxCorners3.hpp
+++ api/math/AxCorners3.hpp
@@ -13,17 +13,35 @@ namespace yq {
     */
     template <typename T>
     struct AxCorners3 {
+
+        //! Component type (captures the argument)
         using component_type    = T;
     
+        //! Lower-lower-lower corner
         T   lll;
+
+        //! Lower-lower-upper corner
         T   llh;
+
+        //! Lower-upper-lower corner
         T   lhl;
+
+        //! Lower-upper-upper corner
         T   lhh;
+
+        //! Upper-lower-lower corner
         T   hll;
+
+        //! Upper-lower-upper corner
         T   hlh;
+
+        //! Upper-upper-lower corner
         T   hhl;
+
+        //! Upper-upper-upper corner
         T   hhh;
         
+        //! Equality operator (defaulted)
         bool operator==(const AxCorners3&) const noexcept = default;
     };
 
