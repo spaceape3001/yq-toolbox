57b593302bbd101b76af6de1158689bf66229451
spaceape3001 <spaceape3001@users.noreply.github.com>
2022-11-27 11:22:43 +0000

    Revert "Altered Math Traits"
    This reverts commit e5c6908ac206095feaab98acc56bed774e5f31d9.

--- api/math/Absolute.hpp
+++ api/math/Absolute.hpp
@@ -7,6 +7,7 @@
 #pragma once
 
 #include <cmath>
+#include <math/trait/has_is_finite.hpp>
 
 namespace yq {
     
@@ -24,5 +25,7 @@ namespace yq {
         {
             return fabs(error) <= threshhold;
         }
+        
+        YQ_IS_FINITE(Absolute, is_finite(v.threshhold) )
     };
 }
