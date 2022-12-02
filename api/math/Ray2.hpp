2b3aec8d27df02a4f9d2a7af5968bd263135b69e
spaceape3001 <spaceape3001@users.noreply.github.com>
2022-12-02 09:56:13 +0000

    Doxygen

--- api/math/Ray2.hpp
+++ api/math/Ray2.hpp
@@ -15,10 +15,17 @@ namespace yq {
     */
     template <typename T>
     struct Ray2 {
+    
+        //! Component type
         using component_type    = T;
+        
+        //! Point on the ray
         Vector2<T>              point;
+        
+        //! Direction of the ray
         Vector2<ieee754_t<T>>   direction;
         
+        //! Defaulted equality operator
         constexpr bool operator==(const Ray2& rhs) const noexcept = default;
     };
     
