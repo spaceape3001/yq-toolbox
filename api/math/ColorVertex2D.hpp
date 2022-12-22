cd6a8e0d7b13a3bf72e575bccd634711bf38bcc0
spaceape3001 <spaceape3001@users.noreply.github.com>
2022-12-22 08:15:56 +0000

    Doxygen

--- api/math/ColorVertex2D.hpp
+++ api/math/ColorVertex2D.hpp
@@ -14,11 +14,20 @@
 namespace yq {
     struct ColorVertexData;
     
+    /*! \brief Used for encapsulating color and position information
+    */
     struct ColorVertex2D {
+    
+        //! Position
         Vector2D        position;
+        
+        //! Color
         RGB3U8          color;
         
+        //! Equality operator
         bool operator==(const ColorVertex2D&) const = default;
+        
+        //! Operator to reduce to color vertex data (GLM)
         operator ColorVertexData() const;
     };
 }
