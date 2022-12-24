55852829b6da3d927e0c4cd616bf6ba178fd84d1
spaceape3001 <spaceape3001@users.noreply.github.com>
2022-12-24 10:16:25 +0000

    Adding class members

--- api/math/AxBox2.hpp
+++ api/math/AxBox2.hpp
@@ -7,6 +7,7 @@
 
 #include <math/preamble.hpp>
 #include <math/Range.hpp>
+#include <math/Size2.hpp>
 #include <math/Vector2.hpp>
 #include <math/AxCorners2.hpp>
 
@@ -27,6 +28,170 @@ namespace yq {
         
         //! Equality operator (defaulted)
         constexpr bool operator==(const AxBox2&) const noexcept = default;
+
+        //! Union
+        constexpr AxBox2 operator|(const AxBox2<T>&b) const noexcept
+        {
+            return { min_elem(lo, b.lo), max_elem(hi, b.hi) };
+        }
+
+        //! Union
+        AxBox2& operator|=(const AxBox2& b) 
+        {
+            lo  = min_elem(lo, b.lo);
+            hi  = max_elem(hi, b.hi);
+            return *this;
+        }
+
+        AxBox2& operator|=(const Vector2<T>& b) 
+        {
+            lo  = min_elem(lo, b);
+            hi  = max_elem(hi, b);
+            return *this;
+        }
+
+        //! \Intersection 
+        constexpr AxBox2 operator&(const AxBox2<T>&b) const noexcept
+        {
+            return { max_elem(lo, b.lo), min_elem(hi, b.hi) };
+        }
+        
+        AxBox2& operator&=(const AxBox2& b) 
+        {
+            lo  = max_elem(lo, b.lo);
+            hi  = min_elem(hi, b.hi);
+            return *this;
+        }
+
+        /*! \brief Computes the area
+        */
+        constexpr square_t<T>               area() const noexcept
+        {
+            return component_product(hi-lo);
+        }
+
+        /*! \brief Computes the center
+        */
+        constexpr Vector2<T>                center() const noexcept
+        {
+            if constexpr (std::is_floating_point_v<T>)
+                return ieee754_t<T>(0.5)*(lo+hi);
+            if constexpr (std::is_integral_v<T>)
+                return (lo+hi) / T(2);
+            return {};
+        }
+
+        
+        constexpr AxCorners2<Vector2<T>>    corners() const noexcept 
+        {
+            return { 
+                southwest(),
+                northwest(),
+                southeast(),
+                northeast()
+            };
+        }
+
+        /*! \brief Checks for full occlusion
+        
+            A small box is "eclipsed" if it's wholy contained (or touching edges) by this box.
+            \param[in] Small The "smaller" box, if eclipsed
+        */
+        constexpr bool          eclipses(const AxBox2<T>& small) const noexcept
+        {
+            return all_less_equal(lo, small.lo) && all_greater_equal(hi, small.hi);
+        }
+
+        /*! \brief Checks if the point is inside (or touching) the box
+        */
+        constexpr bool eclipses(const Vector2<T>& pt) const noexcept
+        {
+            return all_less_equal(lo, pt) && all_less_equal(pt, hi);
+        }
+
+        //! Tests this box for validness
+        constexpr bool          is_valid() const noexcept 
+        {
+            return all_less_equal(lo, hi);
+        }
+            
+        //! Returns the northeast corner of the box
+        constexpr Vector2<T>    northeast() const noexcept { return hi; };
+
+        //! Returns the northwest corner of the box
+        constexpr Vector2<T>    northwest() const noexcept { return { lo.x, hi.y}; }
+        
+        /*! \brief Checks for any overlap
+        
+            This returns TRUE if *ANY* part of the boxes overlap (or touch)
+        */
+        constexpr bool          overlaps(const AxBox2<T>& b) noexcept
+        {
+            return all_less_equal(lo, b.hi) && all_greater_equal(hi, b.lo);
+        }    
+
+        /*! \brief Computes the perimeter of the box */
+        constexpr T             perimeter() const noexcept
+        {
+            return T(2) * component_sum(hi-lo);
+        }
+
+        /*! \brief Projects a local [0,1] coordinate to a global coordinate based on the provided axially aligned box
+        
+            \param[in] v    The local coordinate
+            \return The global coordinate
+        */
+        template <typename=void>
+        requires std::is_floating_point_v<T>
+        constexpr Vector2<T>    project(const Vector2<T>& v) const noexcept
+        {
+            return mul_elem(one_v<Vector2<T>>-v, lo) + mul_elem(v, hi);
+        }
+
+        //! Returns the southeast corner of the box
+        constexpr Vector2<T>    southeast() const noexcept { return { hi.x, lo.y }; }
+        
+        //! Returns the southwest corner of the box
+        constexpr Vector2<T>    southwest() const noexcept { return lo; }
+
+        /*! \brief Returns the span (dimensions) of the box
+        */
+        constexpr Vector2<T>    span() const noexcept
+        {
+            return hi - lo;
+        }
+        
+        /*! \brief Returns the size of the box
+        */
+        constexpr Size2<T>      size() const noexcept
+        {
+            return { x_size(), y_size() };
+        }
+
+        /*! \brief Projects a global coordinate to a local [0,1] coordinate for the axially aligned box
+
+            \param[in] bx   The axially aligned box
+            \param[in] v    The global coordinate
+            \return The local coordinate
+        */
+        template <typename=void>
+        requires std::is_floating_point_v<T>
+        constexpr Vector2<T>   unproject(const Vector2<T>& v) const noexcept
+        {
+            return div_elem(v-lo, span());
+        }
+
+        //! X Range of the box
+        constexpr Range<T>      x_range() const noexcept { return { lo.x, hi.x }; }
+        
+        //! X size of the box
+        constexpr T             x_size() const noexcept { return hi.x - lo.x; }
+        
+        //! Y Range of the box
+        constexpr Range<T>      y_range() const noexcept { return { lo.y, hi.y }; }
+        
+        //! Y size of the box
+        constexpr T             y_size() const noexcept { return hi.y - lo.y; }
     };
 
     YQ_IEEE754_1(AxBox2)
@@ -60,55 +225,50 @@ namespace yq {
     template <typename T>
     constexpr Vector2<T>  northeast(const AxBox2<T>& ax) noexcept
     {
-        return ax.hi;
+        return ax.northeast();
     }
 
     //! Northwest corner of the box
     template <typename T>
     constexpr Vector2<T>  northwest(const AxBox2<T>& ax) noexcept
     {
-        return { ax.lo.x, ax.hi.y };
+        return ax.northwest();
     }
 
     //! Souttheast corner of the box
     template <typename T>
     constexpr Vector2<T>  southeast(const AxBox2<T>& ax) noexcept
     {
-        return { ax.hi.x, ax.lo.y };
-    }    //! Tests for finiteness
+        return ax.southwest();
+    }
 
 
     //! Soutthwest corner of the box
     template <typename T>
     constexpr Vector2<T>  southwest(const AxBox2<T>& ax) noexcept
     {
-        return ax.lo;
+        return ax.southwest();
     }
 
     //! Corners of the box
     template <typename T>
     AxCorners2<Vector2<T>>  corners(const AxBox2<T>& v)
     {
-        return { 
-            v.lo,
-            { v.lo.x, v.hi.y }, 
-            { v.hi.x, v.lo.y }, 
-            v.hi
-        };
+        return v.corners();
     }
 
     //! X Range of the box
     template <typename T>
     constexpr Range<T>  x_range(const AxBox2<T>& v) noexcept
     {
-        return range(v.lo.x, v.hi.x);
+        return v.x_range();
     }
 
     //! Y Range of the box
     template <typename T>
     constexpr Range<T>  y_range(const AxBox2<T>& v) noexcept
     {
-        return range(v.lo.y, v.hi.y);
+        return v.y_range();
     }
 
 //  --------------------------------------------------------
@@ -121,7 +281,7 @@ namespace yq {
     template <typename T>
     constexpr bool    is_valid(const AxBox2<T>& a) noexcept
     {
-        return all_less_equal(a.lo, a.hi);
+        return a.is_valid();
     }
 
 //  --------------------------------------------------------
@@ -176,24 +336,8 @@ namespace yq {
 //  --------------------------------------------------------
 //  UNIONS
 
-    /*! \brief Union of two AABBs
-    */
-    template <typename T>
-    constexpr AxBox2<T> operator|(const AxBox2<T>&a, const AxBox2<T>&b) noexcept
-    {
-        return { min_elem(a.lo, b.lo), max_elem(a.hi, b.hi) };
-    }
-
 //  --------------------------------------------------------
 //  INTERSECTIONS
-
-    /*! \brief Intersection of two AABBs
-    */
-    template <typename T>
-    constexpr AxBox2<T> operator&(const AxBox2<T>&a, const AxBox2<T>&b) noexcept
-    {
-        return { max_elem(a.lo, b.lo), min_elem(a.hi, b.hi) };
-    }
 
 //  --------------------------------------------------------
 //  PROJECTIONS
@@ -208,7 +352,7 @@ namespace yq {
     requires std::is_floating_point_v<T>
     constexpr Vector2<T>   local_to_global(const AxBox2<T>& bx, const Vector2<T>& v) noexcept
     {
-        return mul_elem(one_v<Vector2<T>>-v, bx.lo) + mul_elem(v, bx.hi);
+        return bx.project(v);
     }
 
     /*! \brief Projects a global coordinate to a local [0,1] coordinate for the axially aligned box
@@ -221,7 +365,7 @@ namespace yq {
     requires std::is_floating_point_v<T>
     constexpr Vector2<T>   global_to_local(const AxBox2<T>& bx, const Vector2<T>& v) noexcept
     {
-        return div_elem(v-bx.lo, bx.hi-bx.lo);
+        return bx.unproject(v);
     }
 
 //  --------------------------------------------------------
@@ -232,7 +376,7 @@ namespace yq {
     template <typename T>
     constexpr square_t<T>     area(const AxBox2<T>& ax) noexcept
     {
-        return component_product(ax.hi-ax.lo);
+        return ax.area();
     }
 
     /*! \brief Computes the center of a 2D axially aligned box
@@ -240,12 +384,7 @@ namespace yq {
     template <typename T>
     constexpr Vector2<T>      center(const AxBox2<T>& box) noexcept
     {
-        if constexpr (std::is_floating_point_v<T>){
-            return ieee754_t<T>(0.5)*(box.lo+box.hi);
-        } else if(std::is_integral_v<T>){
-            return (box.lo+box.hi) / T(2);
-        } else
-            return {};
+        return box.center();
     }
     
     /*! \brief Checks for full occlusion
@@ -257,7 +396,7 @@ namespace yq {
     template <typename T>
     constexpr bool is_eclipsed(const AxBox2<T>& big, const AxBox2<T>& small) noexcept
     {
-        return all_less_equal(big.lo, small.lo) && all_greater_equal(big.hi, small.hi);
+        return big.eclipses(small);
     }
     
     /*! \brief Checks if the point is inside (or touching) the box
@@ -265,7 +404,7 @@ namespace yq {
     template <typename T>
     constexpr bool is_inside(const AxBox2<T>& bx, const Vector2<T>& pt) noexcept
     {
-        return all_less_equal(bx.lo, pt) && all_less_equal(pt, bx.hi);
+        return bx.eclipses(pt);
     }
 
     /*! \brief Checks for any overlap
@@ -275,14 +414,14 @@ namespace yq {
     template <typename T>
     constexpr bool is_overlapped(const AxBox2<T>& a, const AxBox2<T>& b) noexcept
     {
-        return all_less_equal(a.lo, b.hi) && all_greater_equal(a.hi, b.lo);
+        return a.overlaps(b);
     }    
 
     /*! \brief Computes the perimeter of the box */
     template <typename T>
     constexpr T       perimeter(const AxBox2<T>& ax) noexcept
     {
-        return 2. * component_sum(ax.hi-ax.lo);
+        return ax.perimeter();
     }
 
     /*! \brief Returns the span (dimensions) of the box
@@ -290,7 +429,7 @@ namespace yq {
     template <typename T>
     constexpr Vector2<T>    span(const AxBox2<T>&a) noexcept
     {
-        return a.hi - a.lo;
+        return a.span();
     }
 }
 
