a05f47b583ff38b330fc1f3694c6d6f4c3a09858
spaceape3001 <spaceape3001@users.noreply.github.com>
2022-12-24 19:16:35 +0000

    Cleanup of AxBox1

--- api/math/AxBox1.hpp
+++ api/math/AxBox1.hpp
@@ -30,13 +30,140 @@ namespace yq {
         
         //! Defaulted equatlity operator
         constexpr bool operator==(const AxBox1&) const noexcept = default;
+
+        /*! \brief Union of two AABBs
+        */
+        constexpr AxBox1<T> operator|(const AxBox1<T>&b) const noexcept
+        {
+            return { min_elem(lo, b.lo), max_elem(hi, b.hi) };
+        }
+
+        //! Union in a box
+        AxBox1& operator|=(const AxBox1& b) 
+        {
+            lo  = min_elem(lo, b.lo);
+            hi  = max_elem(hi, b.hi);
+            return *this;
+        }
+
+        //! Unions in a vector
+        AxBox1& operator|=(const Vector1<T>& b) 
+        {
+            lo  = min_elem(lo, b);
+            hi  = max_elem(hi, b);
+            return *this;
+        }
+
+        /*! \brief Intersection of two AABBs
+        */
+        constexpr AxBox1<T> operator&(const AxBox1<T>&b) const noexcept
+        {
+            return { max_elem(lo, b.lo), min_elem(hi, b.hi) };
+        }
+
+        //! Intersection into self 
+        AxBox1& operator&=(const AxBox1& b) 
+        {
+            lo  = max_elem(lo, b.lo);
+            hi  = min_elem(hi, b.hi);
+            return *this;
+        }
+
+        /*! \brief Computes the center of a 1D axially aligned box
+        */
+        constexpr Vector1<T> center() const noexcept
+        {
+            if constexpr (std::is_floating_point_v<T>)
+                return ieee754_t<T>(0.5)*(lo+hi);
+            if constexpr (std::is_integral_v<T>)
+                return (lo+hi) / T(2);
+            return {};
+        }
+
+        /*! \brief Returns the enumerated corners of the box
+        */
+        constexpr AxCorners1<Vector1<T>>  corners() const noexcept
+        {
+            return { 
+                lo,
+                hi
+            };
+        }
+
+        /*! \brief Checks for full occlusion
+        
+            A small box is "eclipsed" if it's wholy contained (or touching edges) of the bigger box.
+            \param[in] Big   The "bigger" box, if eclipsed
+            \param[in] Small The "smaller" box, if eclipsed
+        */
+        constexpr bool eclipses(const AxBox1<T>& small) const noexcept
+        {
+            return all_less_equal(lo, small.lo) && all_greater_equal(hi, small.hi);
+        }
+
+        /*! \brief Checks if the point is inside (or touching) the box
+        */
+        constexpr bool eclipses(const Vector1<T>& pt) const noexcept
+        {
+            return all_less_equal(lo, pt) && all_less_equal(pt, hi);
+        }
+
+        /*! \brief Tests for a valid box */
+        constexpr bool    is_valid() const noexcept
+        {
+            return all_less_equal(lo, hi);
+        }
+
+        /*! \brief Checks for any overlap
+        
+            This returns TRUE if *ANY* part of the boxes overlap (or touch)
+        */
+        constexpr bool overlaps(const AxBox1<T>& b) const noexcept
+        {
+            return all_less_equal(lo, b.hi) && all_greater_equal(hi, b.lo);
+        }
+
+        /*! \brief Projects a local [0,1] coordinate to a global coordinate based on the provided axially aligned box
+        
+            \param[in] bx   The axially aligned box
+            \param[in] v    The local coordinate
+            \return The global coordinate
+        */
+        template <typename=void>
+        requires std::is_floating_point_v<T>
+        constexpr Vector1<T>   project(const Vector1<T>& v) const noexcept
+        {
+            return mul_elem(one_v<Vector1<T>>-v, lo) + mul_elem(v, hi);
+        }
+
+        constexpr Vector1<T>    span() const noexcept
+        {
+            return hi - lo;
+        }
+
+        /*! \brief Projects a global coordinate to a local [0,1] coordinate for the axially aligned box
+
+            \param[in] bx   The axially aligned box
+            \param[in] v    The global coordinate
+            \return The local coordinate
+        */
+        template <typename=void>
+        requires std::is_floating_point_v<T>
+        constexpr Vector1<T>   unproject(const Vector1<T>& v) const noexcept
+        {
+            return div_elem(v-lo, hi-lo);
+        }
+
+        /*! \brief Returns the x-range of the box
+        */
+        constexpr Range<T>  x_range() const noexcept
+        {
+            return range(lo.x, hi.x);
+        }
     };
     
     YQ_IEEE754_1(AxBox1)
     
-//  --------------------------------------------------------
-//  COMPOSITION
-
     /*! \brief Creates an axially alligned box from one corner vertex
     */
     template <typename T>
@@ -53,198 +180,55 @@ namespace yq {
         return { min_elem(a,b), max_elem(a,b) };
     }
 
+    /*! \brief Creates a 3D axially aligned box from container of Vector2's
+    */
+    template <typename T>
+    AxBox1<T> aabb(const std::vector<Vector1<T>>& vals)
+    {
+        switch(vals.size()){
+        case 0:
+            return {};
+        case 1:
+            return aabb(vals[0]);
+        case 2:
+            return aabb(vals[0],vals[1]);
+        default:
+            break;
+        }
+
+        AxBox1<T>   ret = aabb(vals[0], vals[1]);
+        for(size_t i=2;i<vals.size();++i)
+            ret |= vals[i];
+        return vals;
+    }
+
 
     YQ_NAN_1(AxBox1, { nan_v<Vector1<T>>, nan_v<Vector1<T>>});
     YQ_ZERO_1(AxBox1, { zero_v<Vector1<T>>, zero_v<Vector1<T>>});
 
-//  --------------------------------------------------------
-//  GETTERS
-
-    /*! \brief Returns the enumerated corners of the box
-    */
-    template <typename T>
-    AxCorners1<Vector1<T>>  corners(const AxBox1<T>& v)
-    {
-        return { 
-            v.lo,
-            v.hi
-        };
-    }
-
-    /*! \brief Returns the x-range of the box
-    */
-    template <typename T>
-    constexpr Range<T>  x_range(const AxBox1<T>& v) noexcept
-    {
-        return range(v.lo.x, v.hi.x);
-    }
-
-//  --------------------------------------------------------
-//  BASIC FUNCTIONS
-
     YQ_IS_FINITE_1( AxBox1, is_finite(v.lo) && is_finite(v.hi))
     YQ_IS_NAN_1(AxBox1, is_nan(v.lo) || is_nan(v.hi))
 
-    /*! \brief Tests for a valid box */
-    template <typename T>
-    constexpr bool    is_valid(const AxBox1<T>& a) noexcept
-    {
-        return all_less_equal(a.lo, a.hi);
-    }
-
-//  --------------------------------------------------------
-//  POSITIVE
-
-
-//  --------------------------------------------------------
-//  NEGATIVE
-
-
-//  --------------------------------------------------------
-//  NORMALIZATION
-
-
-//  --------------------------------------------------------
-//  ADDITION
-
-
-//  --------------------------------------------------------
-//  SUBTRACTION
-
-
-//  --------------------------------------------------------
-//  MULTIPLICATION
-
-
-//  --------------------------------------------------------
-//  DIVISION
-
-//  --------------------------------------------------------
-//  POWERS
-
-//  --------------------------------------------------------
-//  DOT PRODUCT
-
-
-//  --------------------------------------------------------
-//  INNER PRODUCT
-
-
-//  --------------------------------------------------------
-//  OUTER PRODUCT
-
-
-//  --------------------------------------------------------
-//  CROSS PRODUCT
-
-
-///  --------------------------------------------------------
-//  OTIMES PRODUCT
-
-//  --------------------------------------------------------
-//  UNIONS
-
-    /*! \brief Union of two AABBs
-    */
-    template <typename T>
-    constexpr AxBox1<T> operator|(const AxBox1<T>&a, const AxBox1<T>&b) noexcept
-    {
-        return { min_elem(a.lo, b.lo), max_elem(a.hi, b.hi) };
-    }
-
-
-//  --------------------------------------------------------
-//  INTERSECTIONS
-
-    /*! \brief Intersection of two AABBs
-    */
-    template <typename T>
-    constexpr AxBox1<T> operator&(const AxBox1<T>&a, const AxBox1<T>&b) noexcept
-    {
-        return { max_elem(a.lo, b.lo), min_elem(a.hi, b.hi) };
-    }
-
-
-
-//  --------------------------------------------------------
-//  PROJECTIONS
-
-    /*! \brief Projects a local [0,1] coordinate to a global coordinate based on the provided axially aligned box
-    
-        \param[in] bx   The axially aligned box
-        \param[in] v    The local coordinate
-        \return The global coordinate
-    */
-    template <typename T>
-    requires std::is_floating_point_v<T>
-    constexpr Vector1<T>   local_to_global(const AxBox1<T>& bx, const Vector1<T>& v) noexcept
-    {
-        return mul_elem(one_v<Vector1<T>>-v, bx.lo) + mul_elem(v, bx.hi);
-    }
-
-    /*! \brief Projects a global coordinate to a local [0,1] coordinate for the axially aligned box
-
-        \param[in] bx   The axially aligned box
-        \param[in] v    The global coordinate
-        \return The local coordinate
-    */
-    template <typename T>
-    requires std::is_floating_point_v<T>
-    constexpr Vector1<T>   global_to_local(const AxBox1<T>& bx, const Vector1<T>& v) noexcept
-    {
-        return div_elem(v-bx.lo, bx.hi-bx.lo);
-    }
-
-//  --------------------------------------------------------
-//  ADVANCED FUNCTIONS
 
     /*! \brief Computes the center of a 1D axially aligned box
     */
     template <typename T>
-    constexpr Vector1<T>      center(const AxBox1<T>& box) noexcept
+    constexpr Vector1<T> center(const AxBox1<T>& box)  noexcept
     {
-        if constexpr (std::is_floating_point_v<T>){
-            return ieee754_t<T>(0.5)*(box.lo+box.hi);
-        } else if(std::is_integral_v<T>){
-            return (box.lo+box.hi) / T(2);
-        } else
-            return {};
+        return box.center();
     }
 
-    /*! \brief Checks for full occlusion
-    
-        A small box is "eclipsed" if it's wholy contained (or touching edges) of the bigger box.
-        \param[in] Big   The "bigger" box, if eclipsed
-        \param[in] Small The "smaller" box, if eclipsed
-    */
+    /*! \brief Tests for a valid box */
     template <typename T>
-    constexpr bool is_eclipsed(const AxBox1<T>& big, const AxBox1<T>& small) noexcept
+    constexpr bool    is_valid(const AxBox1<T>& box) noexcept
     {
-        return all_less_equal(big.lo, small.lo) && all_greater_equal(big.hi, small.hi);
-    }
-
-    /*! \brief Checks if the point is inside (or touching) the box
-    */
-    template <typename T>
-    constexpr bool is_inside(const AxBox1<T>& bx, const Vector1<T>& pt) noexcept
-    {
-        return all_less_equal(bx.lo, pt) && all_less_equal(pt, bx.hi);
-    }
-
-    /*! \brief Checks for any overlap
-    
-        This returns TRUE if *ANY* part of the boxes overlap (or touch)
-    */
-    template <typename T>
-    constexpr bool is_overlapped(const AxBox1<T>& a, const AxBox1<T>& b) noexcept
-    {
-        return all_less_equal(a.lo, b.hi) && all_greater_equal(a.hi, b.lo);
+        return box.is_valid();
     }
 
     template <typename T>
-    constexpr Vector1<T>    span(const AxBox1<T>&a) noexcept
+    constexpr Vector1<T>    span(const AxBox1<T>&box) noexcept
     {
-        return a.hi - a.lo;
+        return box.span();
     }
 }
 
