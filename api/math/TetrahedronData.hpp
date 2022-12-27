87b6d1c0706d2b4241f4c992e139af9678e43cd8
spaceape3001 <spaceape3001@users.noreply.github.com>
2022-12-27 06:16:13 +0000

    Doxygen

--- api/math/TetrahedronData.hpp
+++ api/math/TetrahedronData.hpp
@@ -7,10 +7,18 @@
 #pragma once
 
 namespace yq {
+
+    /*! \brief Generic structure for data of a tetrahedron
+    */
     template <typename V>
     struct TetrahedronData {
+        //! Capture the template parameter
+        using data_type = V;
+        
+        //! The data
         V     a, b, c, d;
 
+        //! Defaulted comparison
         constexpr bool operator==(const TetrahedronData&) const noexcept = default;
     };
 }
