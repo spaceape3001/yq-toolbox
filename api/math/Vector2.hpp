5197a5d7bb059ecbee4f2e94791a7dc6ebae5ed5
spaceape3001 <spaceape3001@users.noreply.github.com>
2022-12-29 16:32:10 +0000

    Cleanup on Vector1

--- api/math/Vector2.hpp
+++ api/math/Vector2.hpp
@@ -7,7 +7,9 @@
 #pragma once
 
 #define YQ__API__MATH__VECTOR_2__HPP 1
+
 #include <math/preamble.hpp>
+#include <math/Absolute.hpp>
 #include <math/Units.hpp>
 #include <math/trig.hpp>
 
@@ -38,6 +40,137 @@ namespace yq {
         constexpr operator glm::vec<2, T, glm::defaultp>() const noexcept
         {
             return { x, y  };
+        }
+        
+        //! Affirmation (positive) operator
+        constexpr Vector2<T> operator+() const noexcept
+        {
+            return *this;
+        }
+
+        //! Negation (negative) operator
+        constexpr Vector2<T> operator-() const noexcept
+        {
+            return {-x,-y};
+        }
+
+        //! TRUE if every component of this vector is greater to b
+        constexpr bool  agreat(const Vector2&b) const noexcept
+        {
+            return (x>b.x) && (y>b.y);
+        }
+
+        //! TRUE if every component of this vector is greater or equal to b
+        constexpr bool  agequal(const Vector2&b) const noexcept
+        {
+            return (x>=b.x) && (y>=b.y);
+        }
+
+        //! TRUE if every component of this vector is less than b
+        constexpr bool  aless(const Vector2&b) const noexcept
+        {
+            return (x<b.x) && (y<b.y);
+        }
+
+        //! TRUE if every component of a this vector is less than (or equal to) b
+        constexpr bool  alequal(const Vector2&b) const noexcept
+        {
+            return (x<=b.x) && (y<=b.y);
+        }
+        
+        //! TRUE if the second vector is CLOSE to this vector, as defined by the comparison operator
+        template <typename R=Absolute>
+        constexpr bool close(const Vector2&b, R compare) const 
+        {
+            return compare((*this-b).length(), b.length());
+        }
+
+        //! Returns the most positive component (as defined by the max function)
+        constexpr T     cmax() const noexcept
+        {
+            return max(x, y);
+        }
+
+        //! Returns the most negative component (as defined by the min function)
+        constexpr T     cmin() const noexcept
+        {
+            return min(x, y);
+        }
+    
+        //! Returns the product of the components
+        constexpr square_t<T>     cproduct() const noexcept
+        {
+            return x*y;
+        }
+
+        //! Returns the sum of the components
+        constexpr T   csum() const noexcept
+        {
+            return x + y;
+        }
+
+        //! Element by element absolute value
+        constexpr Vector2<T>   eabs() const noexcept
+        {
+            return { abs(x), abs(y) };
+        }
+
+        //! Element by element division
+        template <typename U>
+        constexpr Vector2<quotient_t<T,U>>    ediv(const Vector2&b) const noexcept
+        {
+            return {x/b.x, y/b.y};
+        }
+
+        //! Element by element maximum
+        constexpr Vector2<T>   emax(const Vector2&b) const noexcept
+        {
+            return {max(x, b.x), max(y, b.y)};
+        }
+
+        //! Element by element minimum
+        constexpr Vector2<T>   emax(T b) const noexcept
+        {
+            return {max(x, b), max(y, b)};
+        }
+
+        //! Element by element minimum
+        constexpr Vector2<T>   emin(const Vector2&b) const noexcept
+        {
+            return {min(x, b.x), min(y, b.y)};
+        }
+
+        //! Element by element minimum
+        constexpr Vector2<T>   emin(T b) const noexcept
+        {
+            return {min(x, b), min(y, b)};
+        }
+
+        //! Element by element multiplication
+        template <typename U>
+        constexpr Vector2<product_t<T,U>>    emul(const Vector2<U>&b) const noexcept
+        {
+            return {x*b.x, y*b.y};
+        }
+
+        /*! \brief Square of the vector's length
+        
+            This returns the SQUARE of this vector's length.
+        */
+        constexpr square_t<T> length²() const noexcept
+        {
+            return x*x + y*y;
+        }    
+        
+        /*! \brief Length of the vector
+            
+            This returns the length of this vector.
+        */
+        T       length() const
+        {
+            if constexpr (trait::has_sqrt_v<T>)
+                return sqrt(length²());
+            return {};
         }
     };
 
@@ -129,9 +262,9 @@ namespace yq {
         This returns the SQUARE of the given vector's length.
     */
     template <typename T>
-    constexpr square_t<T> length2(const Vector2<T>& a) noexcept
+    constexpr square_t<T> length²(const Vector2<T>& vec) noexcept
     {
-        return a.x*a.x + a.y*a.y;
+        return vec.length²();
     }    
 
     /*! \brief Length of the vector
@@ -142,26 +275,7 @@ namespace yq {
     requires trait::has_sqrt_v<T>
     auto    length(const Vector2<T>& a)
     {
-        return sqrt(length2(a));
-    }
-
-//  --------------------------------------------------------
-//  POSITIVE
-
-    template <typename T>
-    constexpr Vector2<T> operator+(const Vector2<T>& a) noexcept
-    {
-        return a;
-    }
-
-
-//  --------------------------------------------------------
-//  NEGATIVE
-
-    template <typename T>
-    constexpr Vector2<T> operator-(const Vector2<T>&a) noexcept
-    {
-        return {-a.x,-a.y};
+        return sqrt(length²(a));
     }
 
 
@@ -239,9 +353,9 @@ namespace yq {
     }
     
     template <typename T, typename U>
-    constexpr Vector2<product_t<T,U>>    mul_elem(const Vector2<T>&a, const Vector2<T>&b) noexcept
+    constexpr Vector2<product_t<T,U>>    mul_elem(const Vector2<T>&a, const Vector2<U>&b) noexcept
     {
-        return {a.x*b.x, a.y*b.y};
+        return a.emul(b);
     }
 
 //  --------------------------------------------------------
@@ -251,7 +365,7 @@ namespace yq {
     requires (std::is_arithmetic_v<T>)
     constexpr  Vector2<quotient_t<T,U>> operator/(T a, const  Vector2<U>&b) noexcept
     {
-        return (a*b) / length2(b);
+        return (a*b) / length²(b);
     }
 
 
@@ -272,9 +386,9 @@ namespace yq {
     }
 
     template <typename T, typename U>
-    constexpr Vector2<quotient_t<T,U>>    div_elem(const Vector2<T>&a, const Vector2<T>&b) noexcept
+    constexpr Vector2<quotient_t<T,U>>    div_elem(const Vector2<T>&a, const Vector2<U>&b) noexcept
     {
-        return {a.x/b.x, a.y/b.y};
+        return a.ediv(b);
     }
 
 
@@ -333,35 +447,35 @@ namespace yq {
     template <typename T>
     constexpr Vector2<T>   abs_elem(const Vector2<T>&a) noexcept
     {
-        return { abs(a.x), abs(a.y) };
+        return a.eabs();
     }
 
     //! TRUE if every component of a is greater than b
     template <typename T>
     constexpr bool        all_greater(const Vector2<T>& a, const Vector2<T>&b) noexcept
     {
-        return (a.x>b.x) && (a.y>b.y);
+        return a.agreat(b);
     }
 
     //! TRUE if every component of a is greater or equal to b
     template <typename T>
     constexpr bool        all_greater_equal(const Vector2<T>& a, const Vector2<T>&b) noexcept
     {
-        return (a.x>=b.x) && (a.y>=b.y);
+        return a.agequal(b);
     }
 
     //! TRUE if every component of a is less than b
     template <typename T>
     constexpr bool        all_less(const Vector2<T>& a, const Vector2<T>&b) noexcept
     {
-        return (a.x<b.x) && (a.y<b.y);
+        return a.aless(b);
     }
 
     //! TRUE if every component of a is less than (or equal to) b
     template <typename T>
     constexpr bool        all_less_equal(const Vector2<T>& a, const Vector2<T>&b) noexcept
     {
-        return (a.x<=b.x) && (a.y<=b.y);
+        return a.alequal(b);
     }
 
     template <typename T>
@@ -428,25 +542,25 @@ namespace yq {
     template <typename T>
     constexpr T             component_max(const Vector2<T>&a) noexcept
     {
-        return max(a.x, a.y);
+        return a.cmax();
     }
 
     template <typename T>
     constexpr T             component_min(const Vector2<T>&a) noexcept
     {
-        return min(a.x, a.y);
+        return a.cmin();
     }
 
     template <typename T>
     constexpr square_t<T>     component_product(const Vector2<T>& a) noexcept
     {
-        return a.x*a.y;
+        return a.cproduct();
     }
 
     template <typename T>
     constexpr T   component_sum(const Vector2<T>& a) noexcept
     {
-        return a.x + a.y;
+        return a.csum();
     }
 
     /*! \brief "Delta Area"
@@ -475,13 +589,13 @@ namespace yq {
     template <typename T>
     constexpr Vector2<T>   max_elem(const Vector2<T>&a, const Vector2<T>&b) noexcept
     {
-        return {max(a.x, b.x) && max(a.y, b.y)};
+        return a.emax(b);
     }
 
     template <typename T>
     constexpr Vector2<T>   min_elem(const Vector2<T>&a, const Vector2<T>&b) noexcept
     {
-        return {min(a.x, b.x) && min(a.y, b.y)};
+        return a.emin(b);
     }
 
 
