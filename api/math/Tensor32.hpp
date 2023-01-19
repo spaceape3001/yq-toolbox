8480c4a64793553060e0588c9fef9db263f9fd13
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-01-19 07:30:22 +0000

    Cleanup Tensor32

--- api/math/Tensor32.hpp
+++ api/math/Tensor32.hpp
@@ -18,12 +18,153 @@ namespace yq {
     */
     template <typename T>
     struct Tensor32 {
+    
+        //! Component type (captures the template parameter)
         using component_type = T;
+        
         T xx, xy;
         T yx, yy;
         T zx, zy;
 
+        //! Default equality operator
         constexpr bool operator==(const Tensor32&) const noexcept = default;
+
+        constexpr Tensor32  operator+() const noexcept
+        { 
+            return *this; 
+        }
+
+        //! Negation operator
+        constexpr Tensor32  operator-() const noexcept
+        {
+            return {
+                -xx, -xy,
+                -yx, -yy,
+                -zx, -zy
+            };
+        }
+    
+        //  --------------------------------------------------------
+        //  GETTERS
+
+            //! X-column of this tensor
+            constexpr Vector3<T>  x_column() const noexcept
+            {
+                return {xx, yx, zx};
+            }
+
+            //! Y-column of this tensor
+            constexpr Vector3<T>  y_column() const noexcept
+            {
+                return {xy, yy, zy};
+            }
+
+            //! X-row of this tensor
+            constexpr Vector2<T>  x_row() const noexcept
+            {
+                return {xx, xy};
+            }
+
+            //! Y-row of this tensor
+            constexpr Vector2<T>  y_row() const noexcept
+            {
+                return {yx, yy};
+            }
+
+            //! Z-row of this tensor
+            constexpr Vector2<T>  z_row() const noexcept
+            {
+                return {zx, zy};
+            }
+
+        //  --------------------------------------------------------
+        //  SETTERS
+
+            //! Set x-column
+            Tensor32& x_column(const Vector3<T>& v)
+            {
+                xx = v.x;
+                yx = v.y;
+                zx = v.z;
+                return *this;
+            }
+
+            //! Set x-column
+            Tensor32& x_column(T _xx, T _yx, T _zx)
+            {
+                xx = _xx;
+                yx = _yx;
+                zx = _zx;
+                return *this;
+            }
+
+            //! Set y-column
+            Tensor32& y_column(const Vector3<T>& v)
+            {
+                xy = v.x;
+                yy = v.y;
+                zy = v.z;
+                return *this;
+            }
+
+            //! Set y-column
+            Tensor32& y_column(T _xy, T _yy, T _zy)
+            {
+                xy = _xy;
+                yy = _yy;
+                zy = _zy;
+                return *this;
+            }
+
+            //! Set x-row
+            Tensor32& x_row(const Vector2<T>& v)
+            {
+                xx = v.x;
+                xy = v.y;
+                return *this;
+            }
+
+            //! Set x-row
+            Tensor32& x_row(T _xx, T _xy)
+            {
+                xx = _xx;
+                xy = _xy;
+                return *this;
+            }
+
+            //! Set y-row
+            Tensor32& y_row(const Vector2<T>& v)
+            {
+                yx = v.x;
+                yy = v.y;
+                return *this;
+            }
+
+            //! Set y-row
+            Tensor32& y_row(T _yx, T _yy)
+            {
+                yx = _yx;
+                yy = _yy;
+                return *this;
+            }
+
+            //! Set z-row
+            Tensor32& z_row(const Vector2<T>& v)
+            {
+                zx = v.x;
+                zy = v.y;
+                return *this;
+            }
+
+            //! Set z-row
+            Tensor32& z_row(T _zx, T _zy)
+            {
+                zx = _zx;
+                zy = _zy;
+                return *this;
+            }
+
+
     };
 
     YQ_IEEE754_1(Tensor32)
@@ -93,103 +234,37 @@ namespace yq {
 //  GETTERS
 
     template <typename T>
-    constexpr Vector3<T>  x_column(const Tensor32<T>&v) 
+    constexpr Vector3<T>  x_column(const Tensor32<T>&ten) 
     {
-        return {v.xx, v.yx, v.zx};
+        return ten.x_column();
     }
 
     template <typename T>
-    constexpr Vector3<T>  y_column(const Tensor32<T>&v) 
+    constexpr Vector3<T>  y_column(const Tensor32<T>&ten) 
     {
-        return {v.xy, v.yy, v.zy};
+        return ten.y_column();
     }
 
     template <typename T>
-    constexpr Vector2<T>  x_row(const Tensor32<T>&v)
+    constexpr Vector2<T>  x_row(const Tensor32<T>&ten)
     {
-        return {v.xx, v.xy};
+        return ten.x_row();
     }
 
     template <typename T>
-    constexpr Vector2<T>  y_row(const Tensor32<T>&v)
+    constexpr Vector2<T>  y_row(const Tensor32<T>&ten)
     {
-        return {v.yx, v.yy};
+        return ten.y_row();
     }
 
     template <typename T>
-    constexpr Vector2<T>  z_row(const Tensor32<T>&v)
+    constexpr Vector2<T>  z_row(const Tensor32<T>&ten)
     {
-        return {v.zx, v.zy};
+        return ten.z_row();
     }
 
 
-//  --------------------------------------------------------
-//  SETTERS
 
-    template <typename T>
-    Tensor32<T>& set_x_column(Tensor32<T>&ten, const Vector2<T>& v)
-    {
-        ten.xx = v.x;
-        ten.yx = v.y;
-        ten.zx = v.z;
-        return ten;
-    }
-
-    template <typename T>
-    Tensor32<T>& set_y_column(Tensor32<T>&ten, const Vector2<T>& v)
-    {
-        ten.xy = v.x;
-        ten.yy = v.y;
-        ten.zy = v.z;
-        return ten;
-    }
-
-    template <typename T>
-    Tensor32<T>& set_x_row(Tensor32<T>&ten, const Vector3<T>& v)
-    {
-        ten.xx = v.x;
-        ten.xy = v.y;
-        return ten;
-    }
-
-    template <typename T>
-    Tensor32<T>& set_y_row(Tensor32<T>&ten, const Vector3<T>& v)
-    {
-        ten.yx = v.x;
-        ten.yy = v.y;
-        return ten;
-    }
-
-    template <typename T>
-    Tensor32<T>& set_z_row(Tensor32<T>&ten, const Vector3<T>& v)
-    {
-        ten.zx = v.x;
-        ten.zy = v.y;
-        return ten;
-    }
-
-//  --------------------------------------------------------
-//  POSITIVE
-
-    template <typename T>
-    constexpr Tensor32<T>  operator+(const Tensor32<T>& a) 
-    { 
-        return a; 
-    }
-
-
-//  --------------------------------------------------------
-//  NEGATIVE
-
-    template <typename T>
-    constexpr Tensor32<T>  operator-(const Tensor32<T>& a) 
-    {
-        return {
-            -a.xx, -a.xy,
-            -a.yx, -a.yy,
-            -a.zx, -a.zy
-        };
-    }
 
 
 //  --------------------------------------------------------
