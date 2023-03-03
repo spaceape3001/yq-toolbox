e31e60e8891c2b8f021f2b945c9ee8f2be336634
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-03-03 16:55:34 +0000

    Polyline4

--- api/math/Polyline4.hxx
+++ api/math/Polyline4.hxx
@@ -13,13 +13,17 @@
 */
 
 #include <math/Polyline4.hpp>
-#include <math/Segment4.hpp>
+#include <math/PolylineData.hpp>
+#include <math/utility.hpp>
 
 namespace yq {
     template <typename T> Polyline4<T>::Polyline4(const std::vector<Vector4<T>>&pts) : vertex(pts) {}
     template <typename T> Polyline4<T>::Polyline4(std::vector<Vector4<T>>&& pts) : vertex(std::move(pts)) {}
     template <typename T> Polyline4<T>::Polyline4(std::initializer_list<Vector4<T>> pts) : vertex(pts) {}
+    
+    #ifdef YQ_MATH_SEGMENT4_HPP
     template <typename T> Polyline4<T>::Polyline4(const Segment4<T>&s) : Polyline4({s.a, s.b}) {}
+    #endif
 
     template <typename T>
     template <typename U>
@@ -105,27 +109,34 @@ namespace yq {
         return *this;
     }
 
+    #if defined(YQ_MATH_POLYLINE2_HPP) && defined(YQ_MATH_TENSOR_4_2_HPP)
     template <typename T>
         template <typename U>
     Polyline2<product_t<T,U>>   Polyline4<T>::operator*(const Tensor42<U>& b) const
     {
         return Polyline2<product_t<T,U>>(vertex * b);
     }
+    #endif
 
+    #if defined(YQ_MATH_POLYLINE3_HPP) && defined(YQ_MATH_TENSOR_4_3_HPP)
     template <typename T>
         template <typename U>
     Polyline3<product_t<T,U>>   Polyline4<T>::operator*(const Tensor43<U>&b) const
     {
         return Polyline3<product_t<T,U>>(vertex * b);
     }
+    #endif
 
+    #ifdef YQ_MATH_TENSOR_4_4_HPP
     template <typename T>
         template <typename U>
     Polyline4<product_t<T,U>>   Polyline4<T>::operator*(const Tensor44<U>&b) const
     {
         return Polyline4<product_t<T,U>>(vertex * b);
     }
+    #endif
     
+    #ifdef YQ_MATH_TENSOR_4_4_HPP
     template <typename T>
         template <typename U>
     requires self_mul_v<T,U>
@@ -135,6 +146,7 @@ namespace yq {
             v *= b;
         return *this;
     }
+    #endif
 
     template <typename T>
         template <typename U>
@@ -154,11 +166,13 @@ namespace yq {
         return *this;
     }
 
+    #ifdef YQ_MATH_AXBOX4_HPP
     template <typename T>
     constexpr AxBox4<T>   Polyline4<T>::bounds() const noexcept
     {
         return AxBox4<T>(UNION, vertex);
     }
+    #endif
 
     template <typename T>
     T       Polyline4<T>::length() const
@@ -178,14 +192,19 @@ namespace yq {
             return ;
         size_t n = vertex.size() - 1;
         for(size_t i=0;i<n;++i){
+            #ifdef YQ_MATH_SEGMENT4_HPP
             if constexpr (std::is_invocable_v<Pred, Segment4<T>>){
                 pred(Segment4<T>(vertex[i], vertex[i+1]));
-            } else if constexpr (std::is_invocable_v<Pred, Vector4<T>, Vector4<T>>){
+            } else 
+            #endif
+            
+            if constexpr (std::is_invocable_v<Pred, Vector4<T>, Vector4<T>>){
                 pred(vertex[i], vertex[i+1]);
             }
         }
     }
 
+    #ifdef YQ_MATH_SEGMENT4_HPP
     template <typename T>
     std::vector<Segment4<T>>    Polyline4<T>::segments() const
     {
@@ -196,6 +215,7 @@ namespace yq {
         });
         return ret;
     }
+    #endif
 
     //  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     //  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -207,11 +227,13 @@ namespace yq {
         return Polyline4<product_t<T,U>>( a*b.vertex );
     }
 
+    #ifdef YQ_MATH_AXBOX4_HPP
     template <typename T>
     AxBox4<T>   aabb(const Polyline4<T>&poly)
     {
         return poly.bounds();
     }
+    #endif
 
     template <typename T>
     T       length(const Polyline4<T>& poly)
@@ -237,9 +259,11 @@ namespace yq {
         return Polyline4<T>(std::move(pts));
     }
 
+    #ifdef YQ_MATH_SEGMENT4_HPP
     template <typename T>
     Polyline4<T> polyline(const Segment4<T>&seg)
     {
         return Polyline4<T>(seg);
     }
+    #endif
 }
