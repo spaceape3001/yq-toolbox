7ad82d68d5f39d1638c268dae28abce00d9900c5
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-03-26 08:39:21 +0000

    Reorg p2
    Last tweak of the reorg

--- api/math/units.hpp
+++ api/math/units.hpp
@@ -10,6 +10,8 @@
     Convienence header for units
 */
 
+#include <math/unit/Dimension.hpp>
+#include <math/unit/dims.hpp>
 #include <math/unit/declare.hpp>
 #include <math/unit/MKS.hpp>
 #include <math/unit/SCALED.hpp>
