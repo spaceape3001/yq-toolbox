fb4307142cb79a6a964ea13761c15b32f1fd8f8b
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-04-02 07:19:24 +0000

    Adding Hash
    Adding the Fowler Noll Vo Hash Function

--- api/basic/128-bit.hpp
+++ api/basic/128-bit.hpp
@@ -8,16 +8,23 @@
 
 #include <cstdint>
 
+    //  Linux suppourts int128
+using uint128_t = unsigned __int128;
+using int128_t  = __int128;
+
 namespace yq {
         //  UINT 128
     struct UInt128 { 
         uint64_t low, high; 
-        UInt128() : low{}, high{} {}
-        UInt128(uint64_t l, uint64_t h) : low(l), high(h) {}
-        operator unsigned __int128 () const { return *(const unsigned __int128*) this; }
+        constexpr UInt128() noexcept : low{}, high{} {}
+        constexpr UInt128(uint64_t l, uint64_t h) noexcept : low(l), high(h) {}
+        constexpr operator unsigned __int128 () const noexcept { return *(const unsigned __int128*) this; }
     };
+    
+    // helper until we have 128 bit literals
+    inline constexpr uint128_t u128(uint64_t hi, uint64_t lo) noexcept
+    {
+        return (((uint128_t)hi) << 64) | (uint128_t) lo;
+    }
 }
 
-    //  Linux suppourts int128
-using uint128_t = unsigned __int128;
-using int128_t  = __int128;
