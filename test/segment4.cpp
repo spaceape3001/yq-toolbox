891a87468d929d5e4f73a1086eef1e58904eff99
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-04-29 06:11:41 +0000

    Added to Segment4 Unit Test
    Tests to Segment4 unit test

--- test/segment4.cpp
+++ test/segment4.cpp
@@ -7,7 +7,11 @@
 #include <boost/ut.hpp>
 #include <math/Absolute.hpp>
 #include <math/utility.hpp>
+#include <math/shape/Segment4.hpp>
+#include <math/vector/Vector4.hpp>
+
 #include <math/shape/Segment4.hxx>
+#include <math/vector/Vector4.hxx>
 
 namespace ut = boost::ut;
 using namespace ut;
@@ -16,6 +20,24 @@ using namespace yq;
 Absolute tol{1e-12};
 
 ut::suite tests = []{
+    "zero is zero"_test = []{
+        expect( true == (Segment4D(ZERO) == Segment4D(ZERO) ));
+    };
+
+    "add"_test = []{
+        Segment4I       a( Vector4I(ALL,-1), Vector4I(ALL,1));
+        Vector4I        b(X);
+        Segment4I       c( Vector4I(0,-1,-1,-1), Vector4I(2,1,1,1));
+        Segment4I   d   = a + b;
+        Segment4I   e = b + a;
+        expect(true == (d == c));
+        expect(true == (e == c));
+    };
+    
+    "length²"_test = []{
+        Segment4I       a( Vector4I(ALL,-1), Vector4I(ALL,1));
+        expect(16 == a.length²());
+    };
 
 };
 
