afe6f17b6c4f1e85245d5756099599887f68ffed
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-05-07 06:55:20 +0000

    More Keywords

--- api/basic/keywords.hpp
+++ api/basic/keywords.hpp
@@ -13,12 +13,20 @@
 namespace yq {
         //  keywords
     struct all_t {};
+    struct copy_t {};
+    struct move_t {};
     struct no_throw_t {};
     struct parse_t {};
+    struct ref_t {};
+    struct span_t {};
     struct throw_t {};
 
-    static constexpr const all_t            ALL;
+    static constexpr const all_t        ALL;
+    static constexpr const copy_t       COPY;
+    static constexpr const move_t       MOVE;
     static constexpr const no_throw_t   NO_THROW;
     static constexpr const parse_t      PARSE;
+    static constexpr const ref_t        REF;
+    static constexpr const span_t       SPAN;
     static constexpr const throw_t      THROW;
 }
