9a7001cd65fb0f63ea7263b77413ee3e37e7fff8
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-05-13 15:07:39 +0000

    UV & UVW All Construcotrs
    Adding all constructors to the UV & UVW structures.

--- api/math/UV.hpp
+++ api/math/UV.hpp
@@ -6,14 +6,17 @@
 
 #pragma once
 
+#include <basic/keywords.hpp>
+
 namespace yq {
     template <typename T>
     struct UV {
         T   u;
         T   v;
 
-        constexpr const UV() noexcept = default;
-        constexpr const UV(const T& _u, const T& _v) noexcept : u(_u), v(_v) {}
+        constexpr UV() noexcept = default;
+        constexpr UV(const T& _u, const T& _v) noexcept : u(_u), v(_v) {}
+        constexpr UV(all_t, const T& _val) noexcept : u(_val), v(_val) {}
     };
 }
 
--- api/math/UVW.hpp
+++ api/math/UVW.hpp
@@ -6,6 +6,8 @@
 
 #pragma once
 
+#include <basic/keywords.hpp>
+
 namespace yq {
     template <typename T>
     struct UVW {
@@ -13,8 +15,9 @@ namespace yq {
         T   v;
         T   w;
         
-        constexpr const UVW() noexcept = default;
-        constexpr const UVW(const T& _u, const T& _v, const T& _w) noexcept : u(_u), v(_v), w(_w) {}
+        constexpr UVW() noexcept = default;
+        constexpr UVW(const T& _u, const T& _v, const T& _w) noexcept : u(_u), v(_v), w(_w) {}
+        constexpr UVW(all_t, const T& _val) noexcept : u(_val), v(_val), w(_val) {}
     };
 }
 
