86393ab521585e110a6442d3bbf49ca5819e697a
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-05-15 07:12:17 +0000

    Triangle3 Unit Test
    Filling in the triangle3 unit test

--- test/triangle3.cpp
+++ test/triangle3.cpp
@@ -7,9 +7,11 @@
 #include <boost/ut.hpp>
 #include <math/Absolute.hpp>
 #include <math/utility.hpp>
+#include <math/shape/AxBox3.hpp>
 #include <math/shape/Triangle3.hpp>
 #include <math/vector/Vector3.hpp>
 
+#include <math/shape/AxBox3.hxx>
 #include <math/shape/Triangle3.hxx>
 #include <math/vector/Vector3.hxx>
 
@@ -37,6 +39,34 @@ ut::suite tests = []{
     "area"_test = []{
         Triangle3D      a( Vector3D(ZERO), Vector3D(3,4,0), Vector3D(3,4,5));
         expect(is_close(tol, a.area(), 12.5));
+    };
+
+    "bounds"_test = []{
+        Triangle3I      triA(Vector3I(ZERO), Vector3I(0,0,5), Vector3I(5,5,0));
+        AxBox3I         bndA0(Vector3I(0,0,0),Vector3I(5,5,5));
+        AxBox3I         bndA1   = triA.bounds();
+        
+        Triangle3I      triB(Vector3I(ZERO), Vector3I(12,0,3), Vector3I(12,5,-2));
+        AxBox3I         bndB0(Vector3I(0,0,-2),Vector3I(12,5,3));
+        AxBox3I         bndB1 = triB.bounds();
+        
+        expect( true == (bndA0 == bndA1));
+        expect( true == (bndB0 == bndB1));
+
+        expect( false == (bndA1 == bndB1));
+        expect( true  == (bndA1 != bndB1));
+    };
+
+    "edge length²"_test = []{
+        Triangle3I      tri( Vector3I(ZERO), Vector3I(3,4,0), Vector3I(3,4,5));
+        expect( 25 == tri.edge_a_length²());
+        expect( 50 == tri.edge_b_length²());
+        expect( 25 == tri.edge_c_length²());
+    };
+    
+    "perimeter"_test = []{
+        Triangle3D      tri( Vector3D(ZERO), Vector3D(3,4,0), Vector3D(3,4,5));
+        expect(is_close(tol, tri.perimeter(), 17.07106781186548));
     };
 
 };
