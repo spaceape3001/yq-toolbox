2e05da924b265150850475113a38b3d6a8eb54d7
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-05-17 07:26:14 +0000

    Circle2 Unit Tests
    Adding more tests to circle2

--- test/circle2.cpp
+++ test/circle2.cpp
@@ -7,6 +7,12 @@
 #include <boost/ut.hpp>
 #include <math/Absolute.hpp>
 #include <math/utility.hpp>
+
+#include <math/shape/AxBox2.hpp>
+#include <math/shape/Circle2.hpp>
+#include <math/vector/Vector2.hpp>
+
+#include <math/shape/AxBox2.hxx>
 #include <math/shape/Circle2.hxx>
 #include <math/vector/Vector2.hxx>
 
@@ -38,6 +44,33 @@ ut::suite tests = []{
         
         Circle2I    d   = a + b;
         expect(true == (d == c));
+    };
+    
+    "area"_test = []{
+        Circle2D    cirA(Vector2D(4,2),10.);
+        expect( true == is_close(tol, cirA.area(), 314.1592653589793 ));
+    };
+    
+    "bounds"_test = []{
+        Circle2I    cirA(Vector2I(5,6),2);
+
+        AxBox2I     bndA0(Vector2I(3,4),Vector2I(7,8));
+        AxBox2I     bndA1   = cirA.bounds();
+
+        Circle2I    cirB(Vector2I(3,-4), 5);
+        AxBox2I     bndB0(Vector2I(-2,-9),Vector2I(8,1));
+        AxBox2I     bndB1   = cirB.bounds();
+
+        expect( true == (bndA0 == bndA1));
+        expect( true == (bndB0 == bndB1));
+
+        expect( false == (bndA1 == bndB1));
+        expect( true  == (bndA1 != bndB1));
+    };
+    
+    "circumference"_test = []{
+        Circle2D    cirA(Vector2D(4,2),10.);
+        expect( true == is_close(tol, cirA.circumference(), 62.83185307179586 ));
     };
     
     "contains"_test = []{
