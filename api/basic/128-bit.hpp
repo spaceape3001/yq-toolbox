4dd5bc7de3d83d47b36ace87d289da4c9188a115
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-09-22 05:25:18 +0000

    Doxygen

--- api/basic/128-bit.hpp
+++ api/basic/128-bit.hpp
@@ -9,15 +9,25 @@
 #include <cstdint>
 
     //  Linux suppourts int128
+//! 128 bit unsigned integer (gcc native)
 using uint128_t = unsigned __int128;
+
+//! 128 bit signed integer (gcc native)
 using int128_t  = __int128;
 
 namespace yq {
-        //  UINT 128
+    //!  128 bit integer adapter
+    //!  This is an 128 bit integer that can do seemless interaction with 64-bit integers
     struct UInt128 { 
         uint64_t low, high; 
+        
+        //! Default constructor
         constexpr UInt128() noexcept : low{}, high{} {}
+        
+        //! Construct by parts
         constexpr UInt128(uint64_t l, uint64_t h) noexcept : low(l), high(h) {}
+        
+        //! Returns the 128 bit integer
         constexpr operator unsigned __int128 () const noexcept { return *(const unsigned __int128*) this; }
     };
     
