27cb8d75862e10e146ed56e1deefe18016694240
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-10-07 04:10:02 +0000

    Doxygen

--- api/math/Side.hpp
+++ api/math/Side.hpp
@@ -12,12 +12,16 @@
 
 namespace yq {
     
+    //! Simple side flag-like object
+    //!
+    //! This is needed given we want to do more than is available for a simple flags object
     struct Side {
         // Mind-boggling how this gets defined...we're undoing that!
         #ifdef ERROR
         #undef ERROR
         #endif
         
+        //! Our data
         uint8_t bits;
         
         enum : uint8_t {
@@ -46,38 +50,59 @@ namespace yq {
             FAIL        = ERROR|UNKNOWN
         };
 
+        //! Default constructor
         consteval Side() noexcept : bits(0) {}
+        
+        //! Constructs from single value
         constexpr Side(uint8_t b) noexcept : bits(b) {}
+        
+        //! Constructs from two values
         constexpr Side(uint8_t a, uint8_t b) noexcept : bits(a|b) {}
+        
+        //! Constructs from an initializer list
         constexpr Side(std::initializer_list<uint8_t> bs) noexcept : bits(0)
         {
             for(uint8_t u : bs)
                 bits |= u;
         }
+        
+        //! TRUE if this side is a "fail"
         constexpr bool    is_fail() const { return bits & FAIL; }
         
+        //! Defaulted comparison operator
         constexpr bool operator==(const Side&) const noexcept = default;
         
+        //! And-operator of two sides
         constexpr Side operator&(Side b) const noexcept
         {
             return bits & b.bits;
         }
         
+        
+        //! Self-and of two sides
         Side& operator&=(Side b) noexcept
         {
             bits &= b.bits;
             return *this;
         }
 
+        //! Or-operator of two sides
         constexpr Side operator|(Side b) const noexcept
         {
             return bits | b.bits;
         }
 
+        //! Self-or operator of two sides
         Side& operator|=(Side b) noexcept
         {
             bits |= b.bits;
             return *this;
+        }
+        
+        //! Bit inversion
+        constexpr Side operator~() const  noexcept
+        {
+            return { !bits };
         }
     };
 }
