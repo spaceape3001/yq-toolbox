9da366fa38652d328f59034fad877f0442cfda27
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-10-12 05:22:06 +0000

    Doxygen

--- api/math/coord/Cylindrical3.hpp
+++ api/math/coord/Cylindrical3.hpp
@@ -21,29 +21,46 @@ namespace yq {
         static_assert(is_floating_point_v<A> && is_floating_point_v<L>);
         //static_assert(has_sqrt_v<square_t<L>>, "Basically needs trig too");
 
+        //! Angular data
         A       angle;
+        
+        //! Radian data
         L       radius;
+        
+        //! Z data
         L       z;
         
+        //! Default constructor
         constexpr Cylindrical3() noexcept = default;
+        
+        //! Component consturctor
         constexpr Cylindrical3(A _a, L _d, L _z) noexcept : angle(_a), radius(_d), z(_z) {};
 
+        //! Not a number constructor
         template <typename=void> requires (has_nan_v<A> && has_nan_v<L>)
         consteval Cylindrical3(nan_t) noexcept : angle(nan_v<A>), radius(nan_v<L>), z(nan_v<L>) {}
 
+        //! Constructs to a unit size in each coordinate
         consteval Cylindrical3(unit_t) noexcept : angle(zero_v<A>), radius(one_v<L>), z(one_v<L>) {}
+        
+        //! Constructs to a zero
         consteval Cylindrical3(zero_t) noexcept : angle(zero_v<A>), radius(zero_v<L>), z(zero_v<L>) {}
 
         //! Creates a polar assuming euler convention
         Cylindrical3(const Vector3<L>&);
+        
         //! Creates a polar assuming clock convention
         Cylindrical3(clock_arg_t, const Vector3<L>&);
         //! Creates a polar assuming euler convention
         Cylindrical3(euler_t, const Vector3<L>&);
         
+        //! Converts to a L-based 3D vector
         operator Vector3<L> () const;
         
+        //! Constructs a L-based 3D vector using angle as clockwise
         Vector3<L>  operator()(clock_arg_t) const;
+        
+        //! Constructs a L-based 3D vector using angle as counter-clockwise
         Vector3<L>  operator()(euler_t) const;
 
         //! Converts to a vector assuming this polar represents a clock angle
@@ -52,6 +69,7 @@ namespace yq {
         //! Converts to a vector assuming this polar is a typical euler rotation (the default)
         Vector3<L>  to_euler() const;
         
+        //! Equality operator
         constexpr bool    operator==(const Cylindrical3&) const noexcept = default;
         
         constexpr Cylindrical3    operator+() const noexcept;
