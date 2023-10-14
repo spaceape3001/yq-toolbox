678a1b364e34432e851d8c796a220d0d7b15cc61
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-10-14 06:05:13 +0000

    Cleanup

--- api/io/stream/Text.hpp
+++ api/io/stream/Text.hpp
@@ -9,33 +9,28 @@
 #include <io/Stream.hpp>
 #include <string>
 
-namespace yq {
-    class String;
+namespace yq::stream {
+    //! Wrapper to stream to a std::string
+    class Text : public Stream {
+    public:
     
-    namespace stream {
+        //! Constructor that takes in a reference to ta strign
+        //! \note This must remain VALID the entire duration of this class
+        Text(std::string& bytes) : m_bytes(bytes) {}
+        
+        //! Destructor
+        ~Text(){}
+
+        //! Overload that appends to the string
+        bool write(const char* buf, size_t cb) override;
+
+    private:
+        
+        Text(const Text&) = delete;
+        Text(Text&&) = delete;
+        Text& operator=(const Text&) = delete;
+        Text& operator=(Text&&) = delete;
     
-        //! Wrapper to stream to a std::string
-        class Text : public Stream {
-        public:
-        
-            //! Constructor that takes in a reference to ta strign
-            //! \note This must remain VALID the entire duration of this class
-            Text(std::string& bytes) : m_bytes(bytes) {}
-            
-            //! Destructor
-            ~Text(){}
-
-            //! Overload that appends to the string
-            bool write(const char* buf, size_t cb) override;
-
-        private:
-            
-            Text(const Text&) = delete;
-            Text(Text&&) = delete;
-            Text& operator=(const Text&) = delete;
-            Text& operator=(Text&&) = delete;
-        
-            std::string&  m_bytes;
-        };
-    }
+        std::string&  m_bytes;
+    };
 }
