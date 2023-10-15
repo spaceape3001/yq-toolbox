765c800e9f1674a2c1a33351867f0c2dd1509dbd
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-10-15 04:31:53 +0000

    Doxygen

--- api/math/coord/Polar2.hpp
+++ api/math/coord/Polar2.hpp
@@ -15,22 +15,35 @@ namespace yq {
     
         This is a polar coordinate container, assumes the rotation is 
         euler convention (ie, starts at +x, swings through +y, and cycles back around).
+        
+        \tparam A   Angular type
+        \tparam L   Length type
     */
     template <typename A, typename L>
     struct Polar2 {
         static_assert(is_floating_point_v<A> && is_floating_point_v<L>);
         //static_assert(has_sqrt_v<square_t<L>>, "Basically needs trig too");
 
+        //! Angle
         A       angle;
+        
+        //! Radius
         L       radius;
         
+        //! Default constructor
         constexpr Polar2() noexcept = default;
+        
+        //! Component wise constructor
         constexpr Polar2(A _a, L _d) noexcept : angle(_a), radius(_d) {};
 
+        //! NaN constructor
         template <typename=void> requires (has_nan_v<A> && has_nan_v<L>)
         consteval Polar2(nan_t) noexcept : angle(nan_v<A>), radius(nan_v<L>) {}
 
+        //! Unit constructor
         consteval Polar2(unit_t) noexcept : angle(zero_v<A>), radius(one_v<L>) {}
+        
+        //! Zero constructor
         consteval Polar2(zero_t) noexcept : angle(zero_v<A>), radius(zero_v<L>) {}
 
         //! Creates a polar assuming euler convention
@@ -40,9 +53,13 @@ namespace yq {
         //! Creates a polar assuming euler convention
         Polar2(euler_t, const Vector2<L>&);
         
+        //! Convert to x,y cartesian
         operator Vector2<L> () const;
         
+        //! Convert to x,y cartesian with clockwise convention
         Vector2<L>  operator()(clock_arg_t) const;
+
+        //! Convert to x,y cartesian with euler (counter clockwise) convetion
         Vector2<L>  operator()(euler_t) const;
 
         //! Converts to a vector assuming this polar represents a clock angle
@@ -51,6 +68,7 @@ namespace yq {
         //! Converts to a vector assuming this polar is a typical euler rotation (the default)
         Vector2<L>  to_euler() const;
         
+        //! Default equality operator
         constexpr bool    operator==(const Polar2&) const noexcept = default;
         
         constexpr Polar2    operator+() const noexcept;
