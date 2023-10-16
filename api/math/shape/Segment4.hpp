d4f3cc86884ad44f8146737e6b7ba23bd9b90fda
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-10-16 07:41:04 +0000

    Doxygen

--- api/math/shape/Segment4.hpp
+++ api/math/shape/Segment4.hpp
@@ -13,19 +13,35 @@
 
 namespace yq {
 
+
+    /*! \brief Segment in 4D
+    */
     template <typename T>
     struct Segment4 {
         //! Capture the template parameter
         using component_type    = T;
     
-        Vector4<T>  a, b;
         
+        //! First point
+        Vector4<T>  a;
+        
+        //! Second point
+        Vector4<T>  b;
+        
+        //! Default constructor
         constexpr Segment4() noexcept = default;
+        
+        //! Component wise constructor (vector and vector)
         constexpr Segment4(const Vector4<T>& _a, const Vector4<T>& _b) : a(_a), b(_b) {}
+        
+        //! NaN constructor
         template <typename=void> requires has_nan_v<T>
         constexpr Segment4(nan_t) : Segment4( Vector4<T>(NAN), Vector4<T>(NAN)) {}
+        
+        //! Zero constructor
         constexpr Segment4(zero_t) : Segment4( Vector4<T>(ZERO), Vector4<T>(ZERO)) {}
 
+        //! Converter to another segment4 of compatible data types
         template <typename U>
         requires std::is_nothrow_convertible_v<T,U>
         explicit constexpr operator Segment4<U>() const noexcept
@@ -33,6 +49,7 @@ namespace yq {
             return { (Vector4<U>) a, (Vector4<U>) b };
         }
         
+        //! Converter to another segment4 of compatible data types
         template <typename U>
         requires (std::is_convertible_v<T,U> && !std::is_nothrow_convertible_v<T,U>)
         explicit constexpr operator Segment4<U>() const 
@@ -43,51 +60,69 @@ namespace yq {
         //! Defaulted equality
         constexpr bool operator==(const Segment4&) const noexcept = default;
 
+        //! Converts to segment data
         constexpr operator SegmentData<Vector4<T>>() const noexcept;
 
+        //! Affirmation (positive) operator
         constexpr Segment4      operator+() const noexcept;
+        
+        //! Negation operator
         constexpr Segment4      operator-() const noexcept;
         
-        
+        //! Returns the segment displaced by the given amount
         Segment4                operator+(const Vector4<T>&) const noexcept;
+
+        //! Returns the segment anti-displaced by the given amount
         Segment4                operator-(const Vector4<T>&) const noexcept;
         
+        //! Moves the segment by the given amount (ie, added to both a and b)
         Segment4&               operator+=(const Vector4<T>&) noexcept;
+        
+        //! Moves the segment opposite to the given amount
         Segment4&               operator-=(const Vector4<T>&) noexcept;
         
+        //! Scale the segment on both values
         template <typename U>
         requires is_arithmetic_v<U>
         constexpr Segment4<product_t<T,U>>    operator*(U) const noexcept;
         
+        //! Self-scaling for the segment
         template <typename U>
         requires (is_arithmetic_v<U> && self_mul_v<T,U>)
         Segment4<T>&                operator*=(U)  noexcept;
         
-
+        //! Projects segment into one dimension
         template <typename U>
         Segment1<product_t<T,U>>    operator*(const Tensor41<U>&) const noexcept;
 
+        //! Projects segment into two dimensions
         template <typename U>
         Segment2<product_t<T,U>>    operator*(const Tensor42<U>&) const noexcept;
 
+        //! Projects segment into three dimensions
         template <typename U>
         Segment3<product_t<T,U>>    operator*(const Tensor43<U>&) const noexcept;
 
+        //! Projects segment in four dimensions
         template <typename U>
         Segment4<product_t<T,U>>    operator*(const Tensor44<U>&) const noexcept;
         
+        //! Self-projection of segment with tensor
         template <typename U>
         requires self_mul_v<T,U>
         Segment4&                   operator*=(const Tensor44<U>&) noexcept;
 
+        //! Returns a scaled down the segment by the specified amount
         template <typename U>
         requires is_arithmetic_v<U>
         constexpr Segment4<quotient_t<T,U>>   operator/(U) const noexcept;
         
+        //! Self-scaling down this segment by the specified amount
         template <typename U>
         requires (is_arithmetic_v<U> && self_div_v<T,U>)
         Segment4<T>&                operator/=(U)  noexcept;
 
+        //! Bounding box to this segment
         constexpr AxBox4<T>     bounds() const noexcept;
         
         //! Net displacement
@@ -186,6 +221,8 @@ namespace yq {
     YQ_IS_FINITE_1( Segment4, is_finite(v.a) && is_finite(v.b))
     YQ_IS_NAN_1(Segment4, is_nan(v.a) || is_nan(v.b))
     
+    /*! \brief Right side scaling of the segment
+    */
     template <typename T, typename U>
     requires is_arithmetic_v<T>
     constexpr Segment4<product_t<T,U>> operator*(T, const Segment4<U>&) noexcept;
