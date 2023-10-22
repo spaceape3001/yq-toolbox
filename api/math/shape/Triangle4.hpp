1b4f247dd8b5b6fca9574633346e0d1fdcdac6b7
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-10-22 16:16:14 +0000

    Doxygen

--- api/math/shape/Triangle4.hpp
+++ api/math/shape/Triangle4.hpp
@@ -21,15 +21,36 @@ namespace yq {
         //! Capture the template parameter
         using component_type    = T;
     
-        Vector4<T>   a, b, c;
+        //! First point
+        Vector4<T>  a;
 
+        //! Second point
+        Vector4<T>  b;
+
+        //! Third point
+        Vector4<T>  c;
+
+        //! Default constructor
         constexpr Triangle4() noexcept = default;
+
+        //! Component wise constructor
         constexpr Triangle4(const Vector4<T>& _a, const Vector4<T>& _b, const Vector4<T>& _c) noexcept : a(_a), b(_b), c(_c) {}
+
+        //! Constructs from segment and point
+        //! 
+        //! This uses the segment for the first two points, with c being the third point
         constexpr Triangle4(const Segment4<T>&, const Vector4<T>& c) noexcept;
+
+        //! Sets ALL three to the same point value (should have no area)
         constexpr Triangle4(all_t, const Vector4<T>& v) noexcept : a(v), b(v), c(v) {}
+
+        //! Sets all to not-a-number
         consteval Triangle4(nan_t) noexcept : Triangle4(ALL, Vector4<T>(NAN)) {}
+
+        //! Sets all to zero
         consteval Triangle4(zero_t) noexcept : Triangle4(ALL, Vector4<T>(ZERO)) {}
         
+        //! Converts to another triangle of compatible data type
         template <typename U>
         requires std::is_nothrow_convertible_v<T,U>
         explicit constexpr operator Triangle4<U>() const noexcept
@@ -37,6 +58,7 @@ namespace yq {
             return { (Vector4<U>) a, (Vector4<U>) b, (Vector4<U>) c };
         }
         
+        //! Converts to another triangle of compatible data type
         template <typename U>
         requires (std::is_convertible_v<T,U> && !std::is_nothrow_convertible_v<T,U>)
         explicit constexpr operator Triangle4<U>() const 
@@ -50,43 +72,62 @@ namespace yq {
         //! Implicit conversion to triangle data
         constexpr operator TriangleData<Vector4<T>> () const noexcept;
 
+        //! Affirmation (positive) operator
         constexpr Triangle4 operator+() const noexcept;
+
+        //! Negation
         constexpr Triangle4 operator-() const noexcept;
         
+        //! Returns a triangle shifted by the given displacement
         constexpr Triangle4 operator+(const Vector4<T>&) const noexcept;
+
+        //! Displaces this triangle
         Triangle4& operator+=(const Vector4<T>&) noexcept;
+
+        //! Returns a triangle shifted opposite by the given displacement
         constexpr Triangle4 operator-(const Vector4<T>&) const noexcept;
+
+        //! Displaces this triangle away by the given amount
         Triangle4& operator-=(const Vector4<T>&) noexcept;
         
+        //! Returns a scaled triangle
         template <typename U>
         requires is_arithmetic_v<U>
         constexpr Triangle4<product_t<T,U>> operator*(U) const noexcept;
         
+        //! Scales this triangle
         template <typename U>
         requires (is_arithmetic_v<U> && self_mul_v<T,U>)
         Triangle4& operator*=(U) noexcept;
 
+        //! Projects into two dimensions
         template <typename U>
         Triangle2<product_t<T,U>>   operator*(const Tensor42<U>&) const noexcept;
 
+        //! Projects into three dimensions
         template <typename U>
         Triangle3<product_t<T,U>>   operator*(const Tensor43<U>&) const noexcept;
 
+        //! Projects to another four-dimension
         template <typename U>
         Triangle4<product_t<T,U>>   operator*(const Tensor44<U>&) const noexcept;
 
+        //! Self-projects using the tensor
         template <typename U>
         requires self_mul_v<T,U>
         Triangle4&   operator*=(const Tensor44<U>&) noexcept;
 
+        //! Returns a reduced triangle
         template <typename U>
         requires is_arithmetic_v<U>
         constexpr Triangle4<quotient_t<T,U>> operator/(U) const noexcept;
 
+        //! Self-reduces this triangle
         template <typename U>
         requires (is_arithmetic_v<U> && self_div_v<T,U>)
         Triangle4& operator/=(U) noexcept;
         
+        //! Area of this triangle
         square_t<T>             area() const;
 
         /*! \brief Bounding box for this triangle
@@ -95,17 +136,29 @@ namespace yq {
 
         //! Edge opposite the "A" vertex
         constexpr Segment4<T>   edge_a() const noexcept;
+
+        //! Length of the edge-a segment
         constexpr T             edge_a_length() const noexcept;
+
+        //! Length² of the edge-a segment
         constexpr square_t<T>   edge_a_length²() const noexcept;
 
         //! Edge opposite the "B" vertex
         constexpr Segment4<T>   edge_b() const noexcept;
+
+        //! Length of the edge-b segment
         constexpr T             edge_b_length() const noexcept;
+
+        //! Length² of the edge-b segment
         constexpr square_t<T>   edge_b_length²() const noexcept;
 
         //! Edge opposite the "C" vertex
         constexpr Segment4<T>   edge_c() const noexcept;
+
+        //! Length of the edge-c segment
         constexpr T             edge_c_length() const noexcept;
+
+        //! Length² of the edge-c segment
         constexpr square_t<T>   edge_c_length²() const noexcept;
         
         //! Perimeter of this triangel
