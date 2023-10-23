84649d975049fc94a9a445085646a0ab49b8be1e
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-10-23 05:00:53 +0000

    Doxygen

--- api/math/shape/TriangleData.hpp
+++ api/math/shape/TriangleData.hpp
@@ -7,13 +7,28 @@
 #pragma once
 
 namespace yq {
+
+    /*! \brief Data for a triangle
+    */
     template <typename V>
     struct TriangleData {
-        V   a, b, c;
+    
+        //! Data for "A" vertex
+        V   a;
         
+        //! Data for "B" vertex
+        V   b;
+        
+        //! Data for "C" vertex
+        V   c;
+        
+        //! Defaulted equality operator
         constexpr bool operator==(const TriangleData&) const noexcept = default;
     };
 
+
+    //! Creates triangle data from three datas 
+    //  (TODO, preclude Vector1/2/3/4 types)?
     template <typename V>
     TriangleData<V>    triangle(const V& a, const V& b, const V& c)
     {
