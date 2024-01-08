4f2c4e5a8d2f07ef4add60f10b64c52fe2d2a567
spaceape3001 <spaceape3001@users.noreply.github.com>
2024-01-08 06:54:02 +0000

    Doyxgen

--- api/0/math/shape/AxBox3.hpp
+++ api/0/math/shape/AxBox3.hpp
@@ -159,6 +159,7 @@ namespace yq {
         //! Negation operator
         AxBox3  operator-() const noexcept;
 
+        //! Returns a box shifted by the given displacement
         AxBox3  operator+(const Vector3<T>&) const noexcept;
         AxBox3& operator+=(const Vector3<T>&) noexcept;
         AxBox3  operator-(const Vector3<T>&) const noexcept;
