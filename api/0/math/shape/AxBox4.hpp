696c9973f29de99e38114034613f3ca0cdd18b4b
spaceape3001 <spaceape3001@users.noreply.github.com>
2024-01-27 06:18:48 +0000

    Doxygen

--- api/0/math/shape/AxBox4.hpp
+++ api/0/math/shape/AxBox4.hpp
@@ -170,6 +170,7 @@ namespace yq {
         requires is_arithmetic_v<U>
         AxBox4<product_t<T,U>> operator*(U) const noexcept;
 
+        //! Scales this box by the given factor
         template <typename U>
         requires (is_arithmetic_v<U> && self_mul_v<T,U>)
         AxBox4& operator*=(U) noexcept;
