3efc27ce21db6eb474c5a8a695b79c174df5fa6a
spaceape3001 <spaceape3001@users.noreply.github.com>
2024-02-17 08:29:17 +0000

    Doxygen

--- api/0/math/vector/Bivector2.hpp
+++ api/0/math/vector/Bivector2.hpp
@@ -41,6 +41,8 @@ namespace yq {
         
         //! Constructs with a single (possibly) non-zero value in the XY component
         constexpr Bivector2(xy_t, T v) noexcept : xy(v) {}
+        
+        //! Constructs a unit bivector in the XY component
         consteval Bivector2(xy_t) noexcept : Bivector2(XY, one_v<T>) {}
         consteval Bivector2(zero_t) noexcept : Bivector2(ALL, zero_v<T>) {}
         
