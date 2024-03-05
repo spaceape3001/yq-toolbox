9a088c5396e86e5699cbfbfc71af6a78ecb12b18
spaceape3001 <spaceape3001@users.noreply.github.com>
2024-03-05 05:42:31 +0000

    Doxygen

--- api/0/math/vector/Bivector2.hpp
+++ api/0/math/vector/Bivector2.hpp
@@ -122,6 +122,7 @@ namespace yq {
         requires is_arithmetic_v<T>
         constexpr Bivector2<quotient_t<T,U>> operator/(U b) noexcept;
 
+        //! Self-divides this bivector by the given factor
         template <typename U>
         requires (is_arithmetic_v<U> && self_div_v<T,U>)
         Bivector2<T>& operator/=(U b) noexcept;
