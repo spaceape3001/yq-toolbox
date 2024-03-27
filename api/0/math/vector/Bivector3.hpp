b6f51a1fea29dac12abc3ba5d330336d400f6996
spaceape3001 <spaceape3001@users.noreply.github.com>
2024-03-27 08:48:03 +0000

    Doxygen

--- api/0/math/vector/Bivector3.hpp
+++ api/0/math/vector/Bivector3.hpp
@@ -100,6 +100,8 @@ namespace yq {
         
         //! Self-increments this bivector with the other
         Bivector3&                  operator+=(const Bivector3&b) noexcept;
+        
+        //! Adds bivector and multivector
         constexpr Multivector3<T>   operator+(const Multivector3<T>& b) const noexcept;
         constexpr Multivector3<T>   operator+(const Trivector3<T>& b) const noexcept;
         constexpr Multivector3<T>   operator+(const Vector3<T>& b) const noexcept;
