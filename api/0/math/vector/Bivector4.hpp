d9345a89f564c0ce04b43a7f2fc7a10b49938bf5
spaceape3001 <spaceape3001@users.noreply.github.com>
2024-04-21 07:50:20 +0000

    Doxygen

--- api/0/math/vector/Bivector4.hpp
+++ api/0/math/vector/Bivector4.hpp
@@ -57,6 +57,8 @@ namespace yq {
         //! Constructs a bivector with the specified value to the ZW component, all others zero
         constexpr Bivector4(zw_t, T v) noexcept : 
             xy(zero_v<T>), yz(zero_v<T>), zw(zero_v<T>), wx(v), xz(zero_v<T>), yw(zero_v<T>) {}
+
+        //! Constructs a unit bivector in the ZW component
         consteval Bivector4(zw_t) noexcept : Bivector4(ZW, one_v<T>) {}
         constexpr Bivector4(wx_t, T v) noexcept : 
             xy(zero_v<T>), yz(zero_v<T>), zw(zero_v<T>), wx(v), xz(zero_v<T>), yw(zero_v<T>) {}
