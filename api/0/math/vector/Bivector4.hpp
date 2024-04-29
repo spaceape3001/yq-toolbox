7acd1ddfa38e759ff13c0a376f8754f06ef58e5c
spaceape3001 <spaceape3001@users.noreply.github.com>
2024-04-29 07:59:59 +0000

    Doxygen

--- api/0/math/vector/Bivector4.hpp
+++ api/0/math/vector/Bivector4.hpp
@@ -85,6 +85,7 @@ namespace yq {
         //! Constructs a zero bivector
         consteval Bivector4(zero_t) noexcept : Bivector4(ALL, zero_v<T>) {}
 
+        //! Converts to another bivector of compatible type
         template <typename U>
         requires std::is_nothrow_convertible_v<T,U>
         explicit constexpr operator Bivector4<U>() const noexcept
