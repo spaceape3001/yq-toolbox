5db06a53372edbbb7ff461877ec3c0b3fb218a96
spaceape3001 <spaceape3001@users.noreply.github.com>
2024-06-18 09:08:59 +0000

    Actual User Expr Files
    The User expression files (missed on last commit)

---
+++ api/0/math/UserExpr.cpp
@@ -0,0 +1,317 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  YOUR QUILL
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include "UserExpr.hpp"
+
+#include <0/basic/errors.hpp>
+#include <0/basic/Logging.hpp>
+#include <0/basic/TextUtils.hpp>
+#include <tbb/spin_rw_mutex.h>
+#include <stack>
+
+namespace yq::expr {
+
+    static constexpr bool       kDigitsText         = true;
+    static constexpr bool       kPunctStartsText    = true;
+
+    struct Repo {
+        mutable tbb::spin_rw_mutex  mutex;
+        ConMap                      constants;
+        OpMap                       operators;
+        FnMap                       functions;
+        C32Hash                     punctText;
+        
+        Repo()
+        {
+            constants[U"pi"]     = std::numbers::pi_v<double>;
+            constants[U"sqrt2"]  = std::numbers::sqrt2_v<double>;
+            constants[U"sqrt3"]  = std::numbers::sqrt3_v<double>;
+            constants[U"e"]      = std::numbers::e_v<double>;
+            constants[U"ln2"]    = std::numbers::ln2_v<double>;
+            constants[U"ln10"]   = std::numbers::ln10_v<double>;
+            constants[U"egamma"] = std::numbers::egamma_v<double>;
+            punctText.insert(U'_');
+        }
+    };
+    
+    Repo&   repo()
+    {
+        static Repo s_repo;
+        return s_repo;
+    }
+    
+    #define LOCK                                                        \
+        static const Repo&  _r  = repo();                               \
+        tbb::spin_rw_mutex::scoped_lock     _lock(_r.mutex, false);
+    
+    #define WLOCK                                                        \
+        static Repo&  _r  = repo();                                     \
+        tbb::spin_rw_mutex::scoped_lock     _lock(_r.mutex, true);
+
+}
+
+#include "expr/Symbol.ipp"
+
+namespace yq::expr {
+    Expect<InsVector>   compile(const SymVector& syms)
+    {
+        static const Repo&    _r  = repo();
+        
+        InsVector       ret;
+        if(syms.empty())
+            return ret;
+            
+        ret.reserve(syms.size());
+        for(const Symbol& sym : syms){
+            Instruction     ins;
+            std::error_code ec;
+            switch(sym.type){
+            case SymType::None:
+                continue;
+            case SymType::Error:
+                return errors::existing_error();
+            case SymType::Float:
+                ins.data    = *to_double(sym.text);
+                break;
+            default:
+                break;
+            }
+        }
+        
+        return errors::todo();
+    }
+    
+    Expect<InsVector>   compile(std::string_view in)
+    {
+        std::u32string  u32 = to_u32string(in);
+        return compile(u32);
+    }
+    
+    Expect<InsVector>   compile(std::u32string_view in)
+    {
+        Expect<SymVector>   syms    = tokenize(in);
+        if(!syms)
+            return unexpected(syms.error());
+        return compile(*syms);
+    }
+    
+
+    Expect<Any>         constant(std::string_view sv)
+    {
+        std::u32string      u32 = to_u32string(sv);
+        return constant(u32);
+    }
+    
+    Expect<Any>         constant(const std::u32string& k)
+    {
+        {
+            LOCK
+            auto i  = _r.constants.find(k);
+            if(i != _r.constants.end())
+                return i->second;
+        }
+        return errors::bad_argument();
+    }
+
+    Expect<Any>         evaluate(const InsVector& vec, VarMap& vars)
+    {
+        std::stack<Any>     data;
+        
+    }
+
+    void                set_constant(const std::u32string& k, const Any& v)
+    {
+        WLOCK
+        _r.constants[k] = v;
+    }
+    
+    void                set_constant(std::string_view k, const Any&v)
+    {
+        std::u32string u32  = to_u32string(k);
+        set_constant(u32, v);
+    }
+
+    void                set_constant(const std::u32string&k, Any&&v)
+    {
+        WLOCK
+        _r.constants[k] = std::move(v);
+    }
+    
+    void                set_constant(std::string_view k, Any&&v)
+    {
+        std::u32string u32  = to_u32string(k);
+        set_constant(u32, std::move(v));
+    }
+
+
+
+    #undef LOCK
+    #undef WLOCK
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+namespace yq {
+    UserExpr::UserExpr() = default;
+    UserExpr::UserExpr(const UserExpr&) = default;
+    UserExpr::UserExpr(UserExpr&&) = default;
+    UserExpr& UserExpr::operator=(const UserExpr&) = default;
+    UserExpr& UserExpr::operator=(UserExpr&&) = default;
+    UserExpr::~UserExpr() = default;
+}
+
+
+#if 0
+
+#include "expr/Symbol.ipp"
+#include <0/basic/Any.hpp>
+#include <0/basic/errors.hpp>
+#include <tbb/spin_rw_mutex.h>
+
+namespace yq {
+    struct UserExpr::Repo {
+        AnyIgCaseMap                    constants;
+        Grammar                         grammar;
+        mutable tbb::spin_rw_mutex      mutex;
+        
+        Repo()
+        {
+            constants["pi"]     = std::numbers::pi_v<double>;
+            constants["e"]      = std::numbers::e_v<double>;
+            constants["sqrt2"]  = std::numbers::sqrt2_v<double>;
+            constants["sqrt3"]  = std::numbers::sqrt3_v<double>;
+            constants["ln2"]    = std::numbers::ln2_v<double>;
+            constants["ln10"]   = std::numbers::ln10_v<double>;
+            constants["egamma"] = std::numbers::egamma_v<double>;
+        }
+    };
+    
+    UserExpr::Repo& UserExpr::repo()
+    {
+        static Repo s_repo;
+        return s_repo;
+    }
+
+    #define LOCK                                                \
+        const Repo&   _r  = repo();                             \
+        tbb::spin_rw_mutex::scoped_lock _lock(_r.mutex, false);
+    
+    #define WLOCK                                               \
+        Repo&   _r  = repo();                                   \
+        tbb::spin_rw_mutex::scoped_lock _lock(_r.mutex, true);
+    
+
+    
+    Expect<Any>      UserExpr::constant(const std::string& k)
+    {
+        if(k.empty())
+            return errors::empty_key();
+
+        {
+            LOCK
+            auto x = _r.constants.find(k);
+            if(x != _r.constants.end())
+                return x->second;
+        }
+
+        return errors::key_not_found();
+    }
+
+    const UserExpr::Grammar&       UserExpr::default_grammar()
+    {
+        return repo().grammar;
+    }
+
+    std::error_code  UserExpr::set_constant(const std::string&k, Any&& val)
+    {
+        if(k.empty())
+            return errors::empty_key();
+        {
+            WLOCK
+            _r.constants[k] = std::move(val);
+        }
+        return std::error_code();
+    }
+    
+    ////////////////////////////////////////////////////////////////////////////////
+
+    Expect<Any>          UserExpr::execute(std::string_view text, const Grammar&g, Context* ctx)
+    {
+        auto cnd    = parse(text, g);
+        if(!cnd)
+            return unexpected(cnd.error());
+        UserExpr    uex = std::move(*cnd);
+        return uex.evaluate(ctx);
+    }
+
+    Expect<UserExpr>     UserExpr::parse(std::string_view, const Grammar&g)
+    {
+        UserExpr    ret;
+        
+        return errors::todo();
+    }
+    
+    ////////////////////////////////////////////////////////////////////////////////
+    
+    UserExpr::UserExpr() = default;
+    UserExpr::UserExpr(const UserExpr&) = default;
+    UserExpr::UserExpr(UserExpr&&) = default;
+    UserExpr& UserExpr::operator=(const UserExpr&) = default;
+    UserExpr& UserExpr::operator=(UserExpr&&) = default;
+    UserExpr::~UserExpr() = default;
+    
+    ////////////////////////////////////////////////////////////////////////////////
+
+    Expect<Any>         UserExpr::evaluate(Context* ctx) const
+    {
+        return errors::todo();
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////
+
+    UserExpr::Context::Context() = default;
+    UserExpr::Context::Context(const Context&) = default;
+    UserExpr::Context::Context(Context&&) = default;
+    UserExpr::Context& UserExpr::Context::operator=(const Context&) = default;
+    UserExpr::Context& UserExpr::Context::operator=(Context&&) = default;
+    UserExpr::Context::~Context() = default;
+
+    UserExpr::Context::Context(const Options& opts) : m_variables(MaybeCase(opts.varIgCase)), m_options(opts)
+    {
+    }
+
+    Expect<Any> UserExpr::Context::variable(const std::string&k) const
+    {
+        if(k.empty())
+            return errors::empty_key();
+        return errors::todo();
+    }
+    
+    std::error_code UserExpr::Context::set_variable(const std::string&k, Any&&val)
+    {
+        if(k.empty())
+            return errors::empty_key();
+        m_variables[k]   = std::move(val);
+        return std::error_code();
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////
+
+    UserExpr::Grammar::Grammar() = default;
+    UserExpr::Grammar::Grammar(const Grammar&) = default;
+    UserExpr::Grammar::Grammar(Grammar&&) = default;
+    UserExpr::Grammar& UserExpr::Grammar::operator=(const Grammar&) = default;
+    UserExpr::Grammar& UserExpr::Grammar::operator=(Grammar&&) = default;
+    UserExpr::Grammar::~Grammar() = default;
+
+    ////////////////////////////////////////////////////////////////////////////////
+
+
+}
+#endif
+
---
+++ api/0/math/UserExpr.hpp
@@ -0,0 +1,219 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  YOUR QUILL
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include <0/basic/preamble.hpp>
+#include <0/basic/Any.hpp>
+#include <0/basic/Enum.hpp>
+#include <0/basic/MaybeCase.hpp>
+#include <0/math/preamble.hpp>
+#include <0/math/expr/preamble.hpp>
+#include <0/math/expr/Symbol.hpp>
+
+#include <functional>
+#include <iosfwd>
+#include <set>
+#include <string>
+#include <string_view>
+#include <system_error>
+#include <unordered_set>
+#include <vector>
+
+namespace log4cpp { class CategoryStream; }
+
+
+namespace yq::expr {
+    struct Operator;
+    struct Function;
+
+    using u32string_set_t   = std::set<std::u32string>;
+    
+    YQ_ENUM(OpCode,,
+        //! No operation (no-op)
+        None        = 0,
+        
+        //! Assign top item in stack to key in instruction data
+        Assign
+    )
+    
+    struct Instruction {
+        Any                 data;
+        std::u32string      key;
+        void*               exec    = nullptr;
+        OpCode              code    = OpCode::None;
+    };
+
+    using OpMap         = std::map<std::u32string_view, const Operator*>;
+    using C32Hash       = std::unordered_set<char32_t>;
+    using VarMap        = std::map<std::u32string,Any,IgCase>;
+    using ConMap        = std::map<std::u32string,Any,IgCase>;
+    using FnMap         = std::map<std::u32string_view, const Function*, IgCase>;
+    
+    Expect<Any>         evaluate(const InsVector&, VarMap&);
+
+    Expect<InsVector>   compile(const SymVector&);
+    Expect<InsVector>   compile(std::string_view);
+    Expect<InsVector>   compile(std::u32string_view);
+    void                set_constant(const std::u32string&, const Any&);
+    void                set_constant(std::string_view, const Any&);
+    void                set_constant(const std::u32string&, Any&&);
+    void                set_constant(std::string_view, Any&&);
+    Expect<Any>         constant(std::string_view);
+    Expect<Any>         constant(const std::u32string&);
+    
+
+}
+
+
+namespace yq {
+    class UserExpr {
+    public:
+        UserExpr();
+        UserExpr(const UserExpr&);
+        UserExpr(UserExpr&&);
+        UserExpr& operator=(const UserExpr&);
+        UserExpr& operator=(UserExpr&&);
+        ~UserExpr();
+
+        explicit UserExpr(std::string_view);
+        explicit UserExpr(std::u32string_view);
+
+        const std::u32string&   definition() const { return m_definition; }
+        const expr::SymVector&  symbols() const { return m_symbols; }
+        const expr::InsVector&  instructions() const { return m_instructions; }
+        bool                    is_good() const { return m_good; }
+        
+        Expect<Any>     evaluate(expr::VarMap&) const;
+        
+
+    private:
+        expr::SymVector     m_symbols;
+        expr::InsVector     m_instructions;
+        std::u32string      m_definition;
+        bool                m_good          = false;
+        
+        void    _init(std::string_view);
+        void    _init(std::u32string_view);
+    };
+}
+
+#if 0
+
+namespace yq {
+
+    /*! \brief User Expression
+    
+        A user expression is about evaluating a user provided string, 
+        and operating on it.  This won't be as efficient as say compiled
+        or inpreted code, it'll work in a pinch.
+    */
+    class UserExpr {
+    public:
+    
+        //! Sets a system-wide constant (think PI)
+        static Expect<Any>      constant(const std::string& key);
+        static std::error_code  set_constant(const std::string& key, Any&& value);
+    
+        struct Instruction;
+        struct Symbol;
+        class Context;
+        class Grammar;
+        
+        Expect<Any>         evaluate(Context* ctx=nullptr) const;
+        
+        const std::vector<Symbol>&      symbols() const { return m_symbols; }
+        const std::vector<Instruction>& execution() const { return m_execution; }
+        
+        UserExpr();
+        UserExpr(const UserExpr&);
+        UserExpr(UserExpr&&);
+        UserExpr& operator=(const UserExpr&);
+        UserExpr& operator=(UserExpr&&);
+        ~UserExpr();
+
+        const std::string&  definition() const { return m_definition; }
+
+        static const Grammar&       default_grammar();
+        static Expect<UserExpr>     parse(std::string_view, const Grammar&g=default_grammar());
+        
+        //! Shortcut for parse/evaluate
+        static Expect<Any>          execute(std::string_view, const Grammar&g=default_grammar(), Context* ctx=nullptr);
+        
+        enum class Code {
+            NOP     = 0,
+            Value,
+            Operator,
+            Variable,
+            Function,
+            Open,
+            Close
+        };
+
+    private:
+
+        using AnyIgCaseMap  = std::map<std::string,Any,IgCase>;
+        using VarMap        = std::map<std::string,Any,MaybeCase>;
+    
+        std::vector<Symbol>         m_symbols;
+        std::vector<Instruction>    m_execution;
+        std::string                 m_definition;
+    
+        struct Repo;
+        static Repo& repo();
+    };
+    
+    /*! \brief Context
+    
+        Context is a collection of variables/functions/etc 
+    */
+    class UserExpr::Context {
+    public:
+        struct Options {
+            bool    varIgCase   = false;
+        };
+
+        Context();
+        Context(const Options& opts);
+        Context(const Context&);
+        Context(Context&&);
+        Context& operator=(const Context&);
+        Context& operator=(Context&&);
+        ~Context();
+
+        Expect<Any> variable(const std::string&) const;
+        std::error_code set_variable(const std::string&, Any&&);
+        
+    private:
+        
+        VarMap      m_variables;
+        Options     m_options;
+    };
+    
+    class UserExpr::Grammar {
+    public:
+    
+        Grammar();
+        Grammar(const Grammar&);
+        Grammar(Grammar&&);
+        Grammar& operator=(const Grammar&);
+        Grammar& operator=(Grammar&&);
+        ~Grammar();
+    
+    };
+    
+    struct UserExpr::Symbol {
+        std::string_view  text;
+    };
+    
+    struct UserExpr::Instruction {
+        
+        
+        Code            code;
+    };
+}
+
+#endif
