8100094e9fa118d901cf6efbbaec78f243901535
spaceape3001 <spaceape3001@users.noreply.github.com>
2024-09-19 19:55:32 +0000

    Tweaking the SQL Query
    Nice thing like multiple statements and table listing

--- sdk/sqlquery.cpp
+++ sdk/sqlquery.cpp
@@ -29,37 +29,22 @@ struct Column {
     }
 };
 
-int main(int argc, char **argv)
+bool    execute(SqlLite& db, const char* sqlStr)
 {
-    if(argc < 3){
-        std::cerr << "Usage: " << argv[0] << " (database) (sql-statement)\n";
-        return -1;
-    }
-    
-    log_to_std_error();
-
-    SqlLite db;
-    if(!db.open(argv[1])){
-        std::cerr << "Failed to open the specified database.\n";
-        return -1;
-    }
-    
-    SqlQuery sql(db, argv[2]);
+    SqlQuery sql(db, sqlStr);
     if(!sql.valid()){
         std::cerr << "SQL failed to parse.\n";
-        return -1;
+        return false;
     }
 
     SQResult    r;
     int     cc  = sql.column_count();
     if(cc<=0){
         r   = sql.step();
-        if(r == SQResult::Done){
-            std::cout << "Qapla'!\n";
-            return 0;
-        }
+        if(r == SQResult::Done)
+            return true;
         std::cout << "Bad SQL Execution\n";
-        return -1;
+        return false;
     }
     
     Vector<Column>      data;
@@ -109,8 +94,37 @@ int main(int argc, char **argv)
         }
         std::cout << '\n';
     }
+    return true;
+}
+
+int main(int argc, char **argv)
+{
+    if(argc < 2){
+        std::cerr << "Usage: " << argv[0] << " (database) (sql-statement)\n";
+        return -1;
+    }
     
-    std::cout << '\n';
+    log_to_std_error();
+
+    SqlLite db;
+    if(!db.open(argv[1])){
+        std::cerr << "Failed to open the specified database.\n";
+        return -1;
+    }
+    
+    if(argc == 2){
+        auto tables = db.tables();
+        for(auto& s : tables){
+            std::cout << s << '\n';
+        }
+        std::cout << '\n';
+    } else {
+        for(int nq=2;nq<argc;++nq){
+            execute(db, argv[nq]);
+            std::cout << '\n';
+        }
+    }
+    
     std::cout << "Qapla'!\n";
 
     return 0;
