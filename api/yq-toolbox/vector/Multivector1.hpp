f18ef90908c6414874a2ba5451bd2e1d001c9cc8
spaceape3001 <spaceape3001@users.noreply.github.com>
2024-09-20 04:04:53 +0000

    Doxygen

--- api/yq-toolbox/vector/Multivector1.hpp
+++ api/yq-toolbox/vector/Multivector1.hpp
@@ -54,6 +54,8 @@ namespace yq {
         //! Constructs to all NaN
         template <typename=void> requires has_nan_v<T>
         consteval Multivector1(nan_t) noexcept : Multivector1(ALL, nan_v<T>) {}
+        
+        //! Constructs to all ones
         consteval Multivector1(one_t) noexcept : Multivector1(ALL, one_v<T>) {}
         consteval Multivector1(zero_t) noexcept : Multivector1(ALL, zero_v<T>) {}
         
