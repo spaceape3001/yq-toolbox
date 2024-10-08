3e503306b90ffd59b0b2079387930788d87723d8
spaceape3001 <spaceape3001@users.noreply.github.com>
2024-10-08 06:28:26 +0000

    Update ScanLine2

---
+++ api/yq-toolbox/math/Ax.hpp
@@ -0,0 +1,55 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  YOUR QUILL
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+namespace yq {
+
+    //! Axis code... this'll be shared on usage
+    enum class Ax : uint8_t {
+        None = 0,
+        I,
+        J,
+        K,
+        L,
+        M,
+        N,
+        U,
+        V,
+        W,
+        X,
+        Y,
+        Z,
+        
+        Ip = I,
+        Jp = J,
+        Kp = K,
+        Lp = L,
+        Mp = M,
+        Np = N,
+        
+        Up = U,
+        Vp = V,
+        Wp = W,
+        
+        Xp = X,
+        Yp = Y,
+        Zp = Z,
+        
+        In,
+        Jn,
+        Kn,
+        Ln,
+        Mn,
+        Nn,
+        Un,
+        Vn,
+        Wn,
+        Xn,
+        Yn,
+        Zn
+    };
+}
--- api/yq-toolbox/shape/ScanLine2.hpp
+++ api/yq-toolbox/shape/ScanLine2.hpp
@@ -5,21 +5,16 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 #pragma once
+#include <yq-toolbox/math/Ax.hpp>
 
 namespace yq {
-    // Two dimensional representation of a scan along a line of constant X
     template <typename I>
-    struct ScanLine2X {
-        I  x  = {};
-        I  y0 = {};
-        I  y1 = {};
-    };
-
-    // Two dimensional representation of a scan along a line of constant Y
-    template <typename I>
-    struct ScanLine2Y {
-        I  x0 = {};
-        I  x1 = {};
-        I  y  = {};
+    struct ScanLine2 {
+        I           a       = {};
+        I           b0      = {};
+        I           b1      = {}; 
+        
+        //! Axis we're on, valid usages will be I & J
+        Ax          axis    = Ax::None;
     };
 }
