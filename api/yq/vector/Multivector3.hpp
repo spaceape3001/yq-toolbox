7047e6f299447eff37229a36e991a860191116be
spaceape3001 <spaceape3001@users.noreply.github.com>
2024-12-24 06:13:11 +0000

    Doxygen

--- api/yq/vector/Multivector3.hpp
+++ api/yq/vector/Multivector3.hpp
@@ -131,6 +131,8 @@ namespace yq {
         
         //! Self adds multivector to this multivector, returns reference
         Multivector3&           operator+=(const Multivector3& b) noexcept;
+        
+        //! Adds trivector to this multivector, returns result
         constexpr Multivector3  operator+(const Trivector3<T>& b) const noexcept;
         Multivector3&           operator+=(const Trivector3<T>& b) noexcept;
         constexpr Multivector3  operator+(const Vector3<T>&  b) const noexcept;
