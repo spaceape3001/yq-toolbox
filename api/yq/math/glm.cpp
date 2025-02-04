7245dc6cdd12ef00296f03c6475d4d13f78d3164
spaceape3001 <spaceape3001@users.noreply.github.com>
2025-02-04 21:35:35 +0000

    GLM Debug Printing
    Adding debug printing for soem GLM data types.

--- api/yq/math/glm.cpp
+++ api/yq/math/glm.cpp
@@ -10,6 +10,8 @@
 #include <yq/core/DelayInit.hpp>
 #include <yq/core/StreamOps.hpp>
 #include <yq/meta/Init.hpp>
+#include <sstream>
+#include <log4cpp/CategoryStream.hh>
 
 using namespace yq;
 
@@ -99,3 +101,284 @@ static void reg_glm_math () {
 
 YQ_INVOKE(reg_glm_math();)
 
+//  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+yq::Stream& operator<<(yq::Stream&out, const glm::vec2&v)
+{
+    return out << "(" << v.x << "," << v.y << ")";
+}
+
+yq::Stream& operator<<(yq::Stream&out, const glm::vec3&v)
+{
+    return out << "(" << v.x << "," << v.y << "," << v.z << ")";
+}
+
+yq::Stream& operator<<(yq::Stream&out, const glm::vec4&v)
+{
+    return out << "(" << v.x << "," << v.y << "," << v.z << "," << v.w << ")";
+}
+
+yq::Stream& operator<<(yq::Stream&out, const glm::dvec2&v)
+{
+    return out << "(" << v.x << "," << v.y << ")";
+}
+
+yq::Stream& operator<<(yq::Stream&out, const glm::dvec3&v)
+{
+    return out << "(" << v.x << "," << v.y << "," << v.z << ")";
+}
+
+yq::Stream& operator<<(yq::Stream&out, const glm::dvec4&v)
+{
+    return out << "(" << v.x << "," << v.y << "," << v.z << "," << v.w << ")";
+}
+
+yq::Stream& operator<<(yq::Stream&out, const glm::mat2&v)
+{
+    out << '[';
+    out << '[' << v[0][0] << ',' << v[1][0] << ']';
+    out << '[' << v[0][1] << ',' << v[1][1] << ']';
+    out << ']';
+    return out;
+}
+
+yq::Stream& operator<<(yq::Stream&out, const glm::mat3&v)
+{
+    out << '[';
+    out << '[' << v[0][0] << ',' << v[1][0] << ',' << v[2][0] << ']';
+    out << '[' << v[0][1] << ',' << v[1][1] << ',' << v[2][1] << ']';
+    out << '[' << v[0][2] << ',' << v[1][2] << ',' << v[2][2] << ']';
+    out << ']';
+    return out;
+}
+
+yq::Stream& operator<<(yq::Stream&out, const glm::mat4&v)
+{
+    out << '[';
+    out << '[' << v[0][0] << ',' << v[1][0] << ',' << v[2][0] << ',' << v[3][0] << ']';
+    out << '[' << v[0][1] << ',' << v[1][1] << ',' << v[2][1] << ',' << v[3][1] << ']';
+    out << '[' << v[0][2] << ',' << v[1][2] << ',' << v[2][2] << ',' << v[3][2] << ']';
+    out << '[' << v[0][3] << ',' << v[1][3] << ',' << v[2][3] << ',' << v[3][3] << ']';
+    out << ']';
+    return out;
+}
+
+yq::Stream& operator<<(yq::Stream&out, const glm::dmat2&v)
+{
+    out << '[';
+    out << '[' << v[0][0] << ',' << v[1][0] << ']';
+    out << '[' << v[0][1] << ',' << v[1][1] << ']';
+    out << ']';
+    return out;
+}
+
+yq::Stream& operator<<(yq::Stream&out, const glm::dmat3&v)
+{
+    out << '[';
+    out << '[' << v[0][0] << ',' << v[1][0] << ',' << v[2][0] << ']';
+    out << '[' << v[0][1] << ',' << v[1][1] << ',' << v[2][1] << ']';
+    out << '[' << v[0][2] << ',' << v[1][2] << ',' << v[2][2] << ']';
+    out << ']';
+    return out;
+}
+
+yq::Stream& operator<<(yq::Stream&out, const glm::dmat4&v)
+{
+    out << '[';
+    out << '[' << v[0][0] << ',' << v[1][0] << ',' << v[2][0] << ',' << v[3][0] << ']';
+    out << '[' << v[0][1] << ',' << v[1][1] << ',' << v[2][1] << ',' << v[3][1] << ']';
+    out << '[' << v[0][2] << ',' << v[1][2] << ',' << v[2][2] << ',' << v[3][2] << ']';
+    out << '[' << v[0][3] << ',' << v[1][3] << ',' << v[2][3] << ',' << v[3][3] << ']';
+    out << ']';
+    return out;
+}
+
+//  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+std::ostringstream& operator<<(std::ostringstream&out, const glm::vec2&v)
+{
+    out << "(" << v.x << "," << v.y << ")";
+    return out;
+}
+
+std::ostringstream& operator<<(std::ostringstream&out, const glm::vec3&v)
+{
+    out << "(" << v.x << "," << v.y << "," << v.z << ")";
+    return out;
+}
+
+std::ostringstream& operator<<(std::ostringstream&out, const glm::vec4&v)
+{
+    out << "(" << v.x << "," << v.y << "," << v.z << "," << v.w << ")";
+    return out;
+}
+
+std::ostringstream& operator<<(std::ostringstream&out, const glm::dvec2&v)
+{
+    out << "(" << v.x << "," << v.y << ")";
+    return out;
+}
+
+std::ostringstream& operator<<(std::ostringstream&out, const glm::dvec3&v)
+{
+    out << "(" << v.x << "," << v.y << "," << v.z << ")";
+    return out;
+}
+
+std::ostringstream& operator<<(std::ostringstream&out, const glm::dvec4&v)
+{
+    out << "(" << v.x << "," << v.y << "," << v.z << "," << v.w << ")";
+    return out;
+}
+
+std::ostringstream& operator<<(std::ostringstream&out, const glm::mat2&v)
+{
+    out << "[";
+    out << "[" << v[0][0] << "," << v[1][0] << "]";
+    out << "[" << v[0][1] << "," << v[1][1] << "]";
+    out << "]";
+    return out;
+}
+
+std::ostringstream& operator<<(std::ostringstream&out, const glm::mat3&v)
+{
+    out << "[";
+    out << "[" << v[0][0] << "," << v[1][0] << "," << v[2][0] << "]";
+    out << "[" << v[0][1] << "," << v[1][1] << "," << v[2][1] << "]";
+    out << "[" << v[0][2] << "," << v[1][2] << "," << v[2][2] << "]";
+    out << "]";
+    return out;
+}
+
+std::ostringstream& operator<<(std::ostringstream&out, const glm::mat4&v)
+{
+    out << "[";
+    out << "[" << v[0][0] << "," << v[1][0] << "," << v[2][0] << "," << v[3][0] << "]";
+    out << "[" << v[0][1] << "," << v[1][1] << "," << v[2][1] << "," << v[3][1] << "]";
+    out << "[" << v[0][2] << "," << v[1][2] << "," << v[2][2] << "," << v[3][2] << "]";
+    out << "[" << v[0][3] << "," << v[1][3] << "," << v[2][3] << "," << v[3][3] << "]";
+    out << "]";
+    return out;
+}
+
+std::ostringstream& operator<<(std::ostringstream&out, const glm::dmat2&v)
+{
+    out << "[";
+    out << "[" << v[0][0] << "," << v[1][0] << "]";
+    out << "[" << v[0][1] << "," << v[1][1] << "]";
+    out << "]";
+    return out;
+}
+
+std::ostringstream& operator<<(std::ostringstream&out, const glm::dmat3&v)
+{
+    out << "[";
+    out << "[" << v[0][0] << "," << v[1][0] << "," << v[2][0] << "]";
+    out << "[" << v[0][1] << "," << v[1][1] << "," << v[2][1] << "]";
+    out << "[" << v[0][2] << "," << v[1][2] << "," << v[2][2] << "]";
+    out << "]";
+    return out;
+}
+
+std::ostringstream& operator<<(std::ostringstream&out, const glm::dmat4&v)
+{
+    out << "[";
+    out << "[" << v[0][0] << "," << v[1][0] << "," << v[2][0] << "," << v[3][0] << "]";
+    out << "[" << v[0][1] << "," << v[1][1] << "," << v[2][1] << "," << v[3][1] << "]";
+    out << "[" << v[0][2] << "," << v[1][2] << "," << v[2][2] << "," << v[3][2] << "]";
+    out << "[" << v[0][3] << "," << v[1][3] << "," << v[2][3] << "," << v[3][3] << "]";
+    out << "]";
+    return out;
+}
+
+//  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+log4cpp::CategoryStream& operator<<(log4cpp::CategoryStream&out, const glm::vec2&v)
+{
+    return out << "(" << v.x << "," << v.y << ")";
+}
+
+log4cpp::CategoryStream& operator<<(log4cpp::CategoryStream&out, const glm::vec3&v)
+{
+    return out << "(" << v.x << "," << v.y << "," << v.z << ")";
+}
+
+log4cpp::CategoryStream& operator<<(log4cpp::CategoryStream&out, const glm::vec4&v)
+{
+    return out << "(" << v.x << "," << v.y << "," << v.z << "," << v.w << ")";
+}
+
+log4cpp::CategoryStream& operator<<(log4cpp::CategoryStream&out, const glm::dvec2&v)
+{
+    return out << "(" << v.x << "," << v.y << ")";
+}
+
+log4cpp::CategoryStream& operator<<(log4cpp::CategoryStream&out, const glm::dvec3&v)
+{
+    return out << "(" << v.x << "," << v.y << "," << v.z << ")";
+}
+
+log4cpp::CategoryStream& operator<<(log4cpp::CategoryStream&out, const glm::dvec4&v)
+{
+    return out << "(" << v.x << "," << v.y << "," << v.z << "," << v.w << ")";
+}
+
+log4cpp::CategoryStream& operator<<(log4cpp::CategoryStream&out, const glm::mat2&v)
+{
+    out << '[';
+    out << '[' << v[0][0] << ',' << v[1][0] << ']';
+    out << '[' << v[0][1] << ',' << v[1][1] << ']';
+    out << ']';
+    return out;
+}
+
+log4cpp::CategoryStream& operator<<(log4cpp::CategoryStream&out, const glm::mat3&v)
+{
+    out << '[';
+    out << '[' << v[0][0] << ',' << v[1][0] << ',' << v[2][0] << ']';
+    out << '[' << v[0][1] << ',' << v[1][1] << ',' << v[2][1] << ']';
+    out << '[' << v[0][2] << ',' << v[1][2] << ',' << v[2][2] << ']';
+    out << ']';
+    return out;
+}
+
+log4cpp::CategoryStream& operator<<(log4cpp::CategoryStream&out, const glm::mat4&v)
+{
+    out << '[';
+    out << '[' << v[0][0] << ',' << v[1][0] << ',' << v[2][0] << ',' << v[3][0] << ']';
+    out << '[' << v[0][1] << ',' << v[1][1] << ',' << v[2][1] << ',' << v[3][1] << ']';
+    out << '[' << v[0][2] << ',' << v[1][2] << ',' << v[2][2] << ',' << v[3][2] << ']';
+    out << '[' << v[0][3] << ',' << v[1][3] << ',' << v[2][3] << ',' << v[3][3] << ']';
+    out << ']';
+    return out;
+}
+
+log4cpp::CategoryStream& operator<<(log4cpp::CategoryStream&out, const glm::dmat2&v)
+{
+    out << '[';
+    out << '[' << v[0][0] << ',' << v[1][0] << ']';
+    out << '[' << v[0][1] << ',' << v[1][1] << ']';
+    out << ']';
+    return out;
+}
+
+log4cpp::CategoryStream& operator<<(log4cpp::CategoryStream&out, const glm::dmat3&v)
+{
+    out << '[';
+    out << '[' << v[0][0] << ',' << v[1][0] << ',' << v[2][0] << ']';
+    out << '[' << v[0][1] << ',' << v[1][1] << ',' << v[2][1] << ']';
+    out << '[' << v[0][2] << ',' << v[1][2] << ',' << v[2][2] << ']';
+    out << ']';
+    return out;
+}
+
+log4cpp::CategoryStream& operator<<(log4cpp::CategoryStream&out, const glm::dmat4&v)
+{
+    out << '[';
+    out << '[' << v[0][0] << ',' << v[1][0] << ',' << v[2][0] << ',' << v[3][0] << ']';
+    out << '[' << v[0][1] << ',' << v[1][1] << ',' << v[2][1] << ',' << v[3][1] << ']';
+    out << '[' << v[0][2] << ',' << v[1][2] << ',' << v[2][2] << ',' << v[3][2] << ']';
+    out << '[' << v[0][3] << ',' << v[1][3] << ',' << v[2][3] << ',' << v[3][3] << ']';
+    out << ']';
+    return out;
+}
--- api/yq/math/glm.hpp
+++ api/yq/math/glm.hpp
@@ -7,3 +7,46 @@
 #pragma once
 
 #include <yq/config/glm.hpp>
+#include <iosfwd>
+
+namespace log4cpp { class CategoryStream; }
+namespace yq { class Stream; }
+
+std::ostringstream& operator<<(std::ostringstream&, const glm::vec2&);
+std::ostringstream& operator<<(std::ostringstream&, const glm::vec3&);
+std::ostringstream& operator<<(std::ostringstream&, const glm::vec4&);
+std::ostringstream& operator<<(std::ostringstream&, const glm::dvec2&);
+std::ostringstream& operator<<(std::ostringstream&, const glm::dvec3&);
+std::ostringstream& operator<<(std::ostringstream&, const glm::dvec4&);
+std::ostringstream& operator<<(std::ostringstream&, const glm::mat2&);
+std::ostringstream& operator<<(std::ostringstream&, const glm::mat3&);
+std::ostringstream& operator<<(std::ostringstream&, const glm::mat4&);
+std::ostringstream& operator<<(std::ostringstream&, const glm::dmat2&);
+std::ostringstream& operator<<(std::ostringstream&, const glm::dmat3&);
+std::ostringstream& operator<<(std::ostringstream&, const glm::dmat4&);
+
+log4cpp::CategoryStream& operator<<(log4cpp::CategoryStream&, const glm::vec2&);
+log4cpp::CategoryStream& operator<<(log4cpp::CategoryStream&, const glm::vec3&);
+log4cpp::CategoryStream& operator<<(log4cpp::CategoryStream&, const glm::vec4&);
+log4cpp::CategoryStream& operator<<(log4cpp::CategoryStream&, const glm::dvec2&);
+log4cpp::CategoryStream& operator<<(log4cpp::CategoryStream&, const glm::dvec3&);
+log4cpp::CategoryStream& operator<<(log4cpp::CategoryStream&, const glm::dvec4&);
+log4cpp::CategoryStream& operator<<(log4cpp::CategoryStream&, const glm::mat2&);
+log4cpp::CategoryStream& operator<<(log4cpp::CategoryStream&, const glm::mat3&);
+log4cpp::CategoryStream& operator<<(log4cpp::CategoryStream&, const glm::mat4&);
+log4cpp::CategoryStream& operator<<(log4cpp::CategoryStream&, const glm::dmat2&);
+log4cpp::CategoryStream& operator<<(log4cpp::CategoryStream&, const glm::dmat3&);
+log4cpp::CategoryStream& operator<<(log4cpp::CategoryStream&, const glm::dmat4&);
+
+yq::Stream& operator<<(yq::Stream&, const glm::vec2&);
+yq::Stream& operator<<(yq::Stream&, const glm::vec3&);
+yq::Stream& operator<<(yq::Stream&, const glm::vec4&);
+yq::Stream& operator<<(yq::Stream&, const glm::dvec2&);
+yq::Stream& operator<<(yq::Stream&, const glm::dvec3&);
+yq::Stream& operator<<(yq::Stream&, const glm::dvec4&);
+yq::Stream& operator<<(yq::Stream&, const glm::mat2&);
+yq::Stream& operator<<(yq::Stream&, const glm::mat3&);
+yq::Stream& operator<<(yq::Stream&, const glm::mat4&);
+yq::Stream& operator<<(yq::Stream&, const glm::dmat2&);
+yq::Stream& operator<<(yq::Stream&, const glm::dmat3&);
+yq::Stream& operator<<(yq::Stream&, const glm::dmat4&);
