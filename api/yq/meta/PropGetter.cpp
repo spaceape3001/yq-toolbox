d9c1a509cbe8bfb23e60fdf5d14cb4b083e7cbd5
spaceape3001 <spaceape3001@users.noreply.github.com>
2025-02-15 13:53:31 +0000

    Cleanup

--- api/yq/meta/PropGetter.cpp
+++ api/yq/meta/PropGetter.cpp
@@ -11,6 +11,7 @@
 namespace yq {
     PropGetter::PropGetter(PropertyInfo* propInfo, const std::source_location& sl) : Meta("get", propInfo, sl) 
     {
+        assert(propInfo);
         assert("no duplicate getters!" && !propInfo->m_getter);    //  duplicate property is an ERROR
         propInfo->m_getter     = this;
     }
