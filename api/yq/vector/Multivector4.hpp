36239f4e01968884bcb02a7e481fed4ac9c08f34
spaceape3001 <spaceape3001@users.noreply.github.com>
2025-02-20 07:24:05 +0000

    Doxygen

--- api/yq/vector/Multivector4.hpp
+++ api/yq/vector/Multivector4.hpp
@@ -174,6 +174,8 @@ namespace yq {
         
         //! Adds two multivectors together, returns result
         constexpr Multivector4  operator+(const Multivector4& b) const noexcept;
+        
+        //! Adds multivector to this multivector, returns reference
         Multivector4&           operator+=(const Multivector4& b) noexcept;
         constexpr Multivector4  operator+(const Quadvector4<T>& b) const noexcept;
         Multivector4&           operator+=(const Quadvector4<T>& b) noexcept;
