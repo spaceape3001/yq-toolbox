dde9e846f6f669cbfb2bf893404fbd7ac9b36999
spaceape3001 <spaceape3001@users.noreply.github.com>
2025-03-01 06:52:56 +0000

    Doxygen

--- api/yq/vector/Multivector4.hpp
+++ api/yq/vector/Multivector4.hpp
@@ -201,6 +201,8 @@ namespace yq {
         
         //! Subtracts scalar from this multivector, returns reference
         Multivector4&           operator-=(T b) noexcept;
+        
+        //! Subtracts bivector from multivector, returns result
         constexpr Multivector4  operator-(const Bivector4<T>& b) const noexcept;
         Multivector4&           operator-=(const Bivector4<T>& b) noexcept;
         constexpr Multivector4  operator-(const Multivector4& b) const noexcept;
