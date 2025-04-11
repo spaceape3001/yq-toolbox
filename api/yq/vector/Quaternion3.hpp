320b4f35190d5c216f5788f16094bd819bbe8e18
spaceape3001 <spaceape3001@users.noreply.github.com>
2025-04-11 05:42:30 +0000

    Doxygen

--- api/yq/vector/Quaternion3.hpp
+++ api/yq/vector/Quaternion3.hpp
@@ -110,6 +110,7 @@ namespace yq {
         requires std::is_floating_point_v<T>
         Quaternion3(clockwise_k, x_k, MKS<T,dim::Angle>v);
 
+        //! Creates a quaternion that effects a specified counter-clockwise rotation about the y-axis
         template <typename=void>
         requires std::is_floating_point_v<T>
         Quaternion3(ccw_k, y_k, MKS<T,dim::Angle>v);
