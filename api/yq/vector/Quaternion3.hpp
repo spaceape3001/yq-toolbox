c9d81f01738b4637631e5a331a91584fa4b7bb00
spaceape3001 <spaceape3001@users.noreply.github.com>
2025-04-22 04:33:43 +0000

    Doxygen

--- api/yq/vector/Quaternion3.hpp
+++ api/yq/vector/Quaternion3.hpp
@@ -172,6 +172,7 @@ namespace yq {
         }
         #endif
 
+        //! Postive affirmation operator
         Quaternion3             operator+() const;
         Quaternion3             operator-() const;
 
