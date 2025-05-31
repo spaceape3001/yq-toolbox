691462d80e6ebda04245ea19b227a3cbd35d02db
spaceape3001 <spaceape3001@users.noreply.github.com>
2025-05-31 04:05:31 +0000

    Doxygen

--- api/yq/vector/Quaternion3.hpp
+++ api/yq/vector/Quaternion3.hpp
@@ -410,6 +410,8 @@ namespace yq {
     template <typename T, typename R>
     bool is_close(const R& compare, const Quaternion3<T>& actual, std::type_identity_t<T>w, std::type_identity_t<T> x, std::type_identity_t<T> y, std::type_identity_t<T> z);
 
+    //! Streams the quaternion to the stream (generic) in w,x,y,z format
+    //! \tparam S   Stream type
     template <typename S, typename T>
     S&  as_stream(S& s, const Quaternion3<T>& v);
     
