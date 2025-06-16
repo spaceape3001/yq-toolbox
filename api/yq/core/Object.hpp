3e63ac5683cc123021a456f2b039f89781ad3711
spaceape3001 <spaceape3001@users.noreply.github.com>
2025-06-16 07:36:48 +0000

    Object Meta Work
    Light work on the meta, allowing for setting/declarations when things
    aren't working according to plan

--- api/yq/core/Object.hpp
+++ api/yq/core/Object.hpp
@@ -24,6 +24,14 @@ namespace yq {
 */
 #define YQ_OBJECT_FIXER( fixer )     public: template <typename T> using MyFixer    = fixer<T>;
 
+#define YQ_OBJECT_BASE( base )      public: using MyBase    = base;
+
+#define YQ_OBJECT_DECLARE_BASELESS( cls )               \
+public:                                                 \
+    using MyObject = cls;                               \
+    virtual const MyInfo& metaInfo() const override;    \
+    static const MyInfo&  staticMetaInfo();
+
 /*! \brief Declares an object type
 
     Only criteria is that the info is now considered intrinsiic, there's a virtual 
@@ -33,11 +41,8 @@ namespace yq {
     \note   MUST BE USED AT GLOBAL SCOPE (NO NAMESPACES)
 */
 #define YQ_OBJECT_DECLARE( cls, base )                  \
-public:                                                 \
-    using MyBase   = base;                              \
-    using MyObject = cls;                               \
-    virtual const MyInfo& metaInfo() const override;    \
-    static const MyInfo&  staticMetaInfo();
+    YQ_OBJECT_BASE(base)                                \
+    YQ_OBJECT_DECLARE_BASELESS(cls)
 
     template <typename Obj>
     struct ObjectFixer;
@@ -99,6 +104,11 @@ public:                                                 \
         static Info&       edit() { return const_cast<Info&>(Obj::staticMetaInfo()); }
     };
 
+    template <SomeObject T>
+    auto writer(unsafe_k)
+    {
+        return typename T::MyInfo::template Writer<T>( const_cast<typename T::MyInfo&>( T::staticMetaInfo()));
+    }
 }
 
 /*! \brief IMPLEMENTS meta for the specified object
@@ -118,3 +128,24 @@ public:                                                 \
     }                                                                                                               \
     template <> yq::DelayInit::Ctor yq::ObjectFixer<name>::s_reg([](){ name::staticMetaInfo(); });
 
+/*! \brief Force a info binding declaration
+
+    This is useful when there's a diamond pattern forming, to resolve that YES, this 
+    is a known meta-object
+*/
+#define YQ_OBJECT_FORCE(...)                                            \
+    namespace yq {                                                      \
+        template <>                                                     \
+        struct InfoBinder<__VA_ARGS__>  : public std::true_type {       \
+            using Info = __VA_ARGS__::MyInfo;                           \
+            static constexpr const bool Defined         = true;         \
+            static constexpr const bool IsObject        = true;         \
+            static constexpr const bool IsType          = false;        \
+            static constexpr const bool IsCompound      = true;         \
+            static constexpr const bool IsInterface     = false;        \
+            static constexpr const bool IsProxy         = false;        \
+            static constexpr const bool IsTachyon       = false;        \
+            static const Info&   bind() { return edit(); }              \
+            static Info&         edit()  { return const_cast<Info&>(__VA_ARGS__::staticMetaInfo()); } \                                \
+        };                                                              \
+    } 
