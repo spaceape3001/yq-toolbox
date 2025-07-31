a723c0ed7bb2e5a06371e8f9475aa524e048db88
spaceape3001 <spaceape3001@users.noreply.github.com>
2025-07-31 08:27:03 +0000

    Foreach Utilities
    Utilities to iterate vectors with predicates, and fast return for
    predicates returning non-defaults

---
+++ api/yq/algorithm/foreach_iterable.hpp
@@ -0,0 +1,64 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  YOUR QUILL
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include <span>
+#include <type_traits>
+
+namespace yq {
+    template <typename Base, typename Desired, typename Pred>
+    auto    foreach_iterable(std::span<Base* const> data, Pred&&pred) 
+    {
+        using base_t    = std::remove_cv_t<Base>;
+        using desired_t = std::remove_cv_t<Desired>;
+        static constexpr const bool is_base   = std::is_same_v<base_t, desired_t>;
+        
+        if constexpr ( std::is_invocable_v<Pred, desired_t&> ){
+            using return_t  = std::invoke_result_t<Pred, desired_t&>;
+            static constexpr const bool is_void = std::is_same_v<return_t, void>;
+            for(auto& itr : data){
+                Desired*  val = nullptr;
+                if constexpr (is_base){
+                    val = itr;
+                } else {
+                    val = dynamic_cast<Desired*>(itr);
+                }
+                if(!val)
+                    continue;
+                if constexpr (!is_void){
+                    return_t   ret = pred(*val);
+                    if(ret != return_t{})
+                        return ret;
+                } else
+                    pred(*val);
+            }
+            if constexpr (!is_void)
+                return return_t{};
+        } else if constexpr ( std::is_invocable_v<Pred, desired_t*> ){
+            using return_t  = std::invoke_result_t<Pred, desired_t*>;
+            static constexpr const bool is_void = std::is_same_v<return_t, void>;
+            for(auto& itr : data){
+                Desired*  val = nullptr;
+                if constexpr (is_base){
+                    val = itr;
+                } else {
+                    val = dynamic_cast<Desired*>(itr);
+                }
+                if(!val)
+                    continue;
+                if constexpr (!is_void){
+                    return_t   ret = pred(val);
+                    if(ret != return_t{})
+                        return ret;
+                } else
+                    pred(*val);
+            }
+            if constexpr (!is_void)
+                return return_t{};
+        }
+    }
+}
