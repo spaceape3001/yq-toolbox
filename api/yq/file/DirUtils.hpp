3ad1534a928b4dbbfdf01dec9793ef2d43d4b8b8
spaceape3001 <spaceape3001@users.noreply.github.com>
2025-08-01 06:52:03 +0000

    Doxygen

--- api/yq/file/DirUtils.hpp
+++ api/yq/file/DirUtils.hpp
@@ -49,6 +49,7 @@ namespace yq {
         //!  Finds all children with specfied name (case insensitive) across provided directories
         path_vec_t          all_children(const std::vector<filesystem_path_t>& dir, std::string_view name);
 
+        //! Returns the first child with the specified case insensitive name
         filesystem_path_t           first_child(const filesystem_path_t& dir, std::string_view name);
         filesystem_path_t           first_child(const std::vector<filesystem_path_t>&, std::string_view name);
         
