b37b0d619c7e72a5a26f9527f84ab2864676cec3
spaceape3001 <spaceape3001@users.noreply.github.com>
2025-10-12 07:36:08 +0000

    Color Typedefs
    New color typedefs (for GLTF importing)

--- api/yq/typedef/rgb.hpp
+++ api/yq/typedef/rgb.hpp
@@ -15,6 +15,10 @@ namespace yq {
     using RGB3F                     = RGB<float>;
     using RGB3I                     = RGB<int>;
     using RGB3U                     = RGB<unsigned>;
+    using RGB3I8                    = RGB<int8_t>;
+    using RGB3I16                   = RGB<int16_t>;
+    using RGB3I32                   = RGB<int32_t>;
     using RGB3U8                    = RGB<uint8_t>;
     using RGB3U16                   = RGB<uint16_t>;
+    using RGB3U32                   = RGB<uint32_t>;
 }
--- api/yq/typedef/rgba.hpp
+++ api/yq/typedef/rgba.hpp
@@ -14,7 +14,11 @@ namespace yq {
     using RGBA4D                    = RGBA<double>;
     using RGBA4F                    = RGBA<float>;
     using RGBA4I                    = RGBA<int>;
+    using RGBA4I8                   = RGBA<int8_t>;
+    using RGBA4I16                  = RGBA<int16_t>;
+    using RGBA4I32                  = RGBA<int32_t>;
     using RGBA4U                    = RGBA<unsigned>;
     using RGBA4U8                   = RGBA<uint8_t>;
     using RGBA4U16                  = RGBA<uint16_t>;
+    using RGBA4U32                  = RGBA<uint32_t>;
 }
