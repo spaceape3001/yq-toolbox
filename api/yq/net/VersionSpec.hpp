d8d165a68a7cd7a8778aa22e27343c313c390640
spaceape3001 <spaceape3001@users.noreply.github.com>
2025-10-16 05:19:17 +0000

    Doxygen

--- api/yq/net/VersionSpec.hpp
+++ api/yq/net/VersionSpec.hpp
@@ -73,6 +73,7 @@ namespace yq {
     //! (Needed as the generic streamer doesn't seem to work)
     log4cpp::CategoryStream& operator<<(log4cpp::CategoryStream&, const VersionSpec&);
     
+    //! Technically the helper to stream to a generic stream (doesn't always seem to work, see above)
     template <typename S>
     requires (!std::is_same_v<S, mithril::WebHtml>)
     S&  operator<<(S& s, const VersionSpec&v)
