6b81b8ab6c69da1c549791c3474c4212bb71409d
spaceape3001 <spaceape3001@users.noreply.github.com>
2026-01-01 22:20:17 +0000

    String Literal Touchup
    Explicitly making the string literal null terminated so I can add in a
    c-style accessor

--- api/yq/core/StringLiteral.hpp
+++ api/yq/core/StringLiteral.hpp
@@ -15,16 +15,22 @@
 namespace yq {
     template <size_t N>
     struct StringLiteral {
-        char value[N];
+        char value[N+1];
 
         constexpr StringLiteral(const char (&str)[N])
         {
             std::copy_n(str, N, value);
+            value[N] = '\0';
         }
         
         constexpr std::string_view view() const  noexcept
         {
             return std::string_view(value, N);
+        }
+        
+        constexpr const char* c_str() const 
+        {
+            return value;
         }
     };
 }
