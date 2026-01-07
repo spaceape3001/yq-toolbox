10809770d02c6f6d0e61529568e3ec374029fd27
spaceape3001 <spaceape3001@users.noreply.github.com>
2026-01-07 05:47:11 +0000

    Doxygen

--- api/yq/core/Version.hpp
+++ api/yq/core/Version.hpp
@@ -6,6 +6,8 @@ namespace yq {
     
     //! Simple version structure (major/minor/patch/build)
     struct Version {
+    
+        //! Major version number
         uint16_t    major   = 0;
         uint16_t    minor   = 0;
         uint16_t    patch   = 0;
