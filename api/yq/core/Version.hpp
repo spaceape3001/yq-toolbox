4b50e56f5fe62ee9e05ce6f947f2f79148e73067
spaceape3001 <spaceape3001@users.noreply.github.com>
2026-01-10 07:35:45 +0000

    Doxygen

--- api/yq/core/Version.hpp
+++ api/yq/core/Version.hpp
@@ -5,6 +5,7 @@
 namespace yq {
     
     //! Simple version structure (major/minor/patch/build)
+    //! Uses 16-bit unsigned shorts to stay at 64-bits total
     struct Version {
     
         //! Major version number
@@ -15,6 +16,8 @@ namespace yq {
         
         //! Patch version number
         uint16_t    patch   = 0;
+        
+        //! Build number
         uint16_t    build   = 0;
         
         auto operator<=>(const Version&) const = default;
