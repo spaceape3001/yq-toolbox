70f2b5158a3b4af334abdc19a120d77c7c6f1fd6
spaceape3001 <spaceape3001@users.noreply.github.com>
2026-01-15 21:36:54 +0000

    XG Doc Node Fix

--- api/yq/xg/XGDocNode.hpp
+++ api/yq/xg/XGDocNode.hpp
@@ -11,15 +11,19 @@
 #include <yq/xg/XGBitPri.hpp>
 #include <yq/xg/XGDocBase.hpp>
 #include <yq/xg/XGNodeType.hpp>
+#include <yq/xg/XGSpecType.hpp>
 
 namespace yq {
     struct XGDocNode : public XGDocBase, public XGBitBox, public XGBitPri {
     
         //! If unspecified, will use inherited (either meta or template)
         XGNodeType      node_type   = XGNodeType::Unspecified;
+        
+        XGSpecType      spec_type   = XGSpecType::Unknown;
+        
+        std::string     spec_data;
 
-        //! Node's element meta name (for now, will go template???)
-        std::string     element;
+        std::string     label;
 
     };
 }
