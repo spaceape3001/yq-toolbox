7f5b146b8b80b1044d0188f0bdd00cdab2afc981
spaceape3001 <spaceape3001@users.noreply.github.com>
2026-08-14 15:45:28 +0000

    Doxygen

--- api/yq/core/Flags.hpp
+++ api/yq/core/Flags.hpp
@@ -354,6 +354,7 @@ namespace yq {
             return Flags(m_value ^ mask(e) );
         }
 
+		//! Adds/Unions in specific enum into the LHS flags, returns reference
         Flags&   operator+=(E e) noexcept
         {
             m_value |= mask(e);
