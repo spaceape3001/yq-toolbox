68f1f185a558c41f26d12bfcf5d39cc43cb5e9eb
spaceape3001 <spaceape3001@users.noreply.github.com>
2026-08-20 09:54:52 +0000

    Doxygen

--- api/yq/core/Flags.hpp
+++ api/yq/core/Flags.hpp
@@ -399,6 +399,11 @@ namespace yq {
             return Flags(mask(e) | f.m_value);
         }
 
+		/*! \brief Differences the LHS enum with flags, returns the result
+		
+			\note The effective result will either be the enum set/all-others 
+			clear *IF* the RHS flags bit is set; otherwise, an null flags
+		*/
         friend constexpr Flags        operator-(E e, Flags f) noexcept
         {
             return Flags(mask(e) &~ f.m_value);
